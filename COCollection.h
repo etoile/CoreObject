363a558d5aa443c021ebe6ef3fd985f5eba22a95
Quentin Mathé <quentin.mathe@gmail.com>
2011-12-08 10:21:02 +0000

    Forgot to commit the new COCollection in a previous commit
    git-svn-id: http://svn.gna.org/svn/etoile/trunk/Etoile/Frameworks/ObjectMerging@7406 7b3f36aa-e6db-49c1-8b4c-66eac1997e64

---
+++ COCollection.h
@@ -0,0 +1,74 @@
+/*
+	Copyright (C) 2011 Quentin Mathe
+
+	Author:  Quentin Mathe <quentin.mathe@gmail.com>
+	Date:  December 2011
+	License:  Modified BSD  (see COPYING)
+ */
+
+#import <Foundation/Foundation.h>
+#import <EtoileFoundation/EtoileFoundation.h>
+#import <ObjectMerging/COObject.h>
+
+/**
+ * @group Object Collection and Organization
+ *
+ * COCollection is a abstract class that provides a common API to various 
+ * concrete collection subclasses such as COGroup or COContainer.
+ *
+ * COCollection represents a mutable collection, but subclasses can be immutable.
+ */
+@interface COCollection : COObject <ETCollection, ETCollectionMutation>
+{
+
+}
+
+/** @taskunit Collection Mutation Additions */
+
+/**
+ * Adds all the given objects to the receiver content.
+ */
+- (void)addObjects: (NSArray *)anArray;
+
+/** @taskunit Object Matching */
+
+/**
+ * Returns the first object whose name matches.
+ *
+ * The search is shallow, in other words limited to the objects in the receiver 
+ * content.
+ */
+- (COObject *)objectForName: (NSString *)aName;
+/**
+ * See -[COObjectMatching objectsMatchingQuery:].
+ *
+ * Object graph traversal implementation for COObjectMatching protocol.
+ */
+- (NSArray *)objectsMatchingQuery: (COQuery *)aQuery;
+
+@end
+
+
+/** @group Object Collection and Organization */
+@interface COObject (COCollectionTypeQuerying)
+/**
+ * Returns whether the receiver is a group or not.
+ */
+- (BOOL)isGroup;
+/**
+ * Returns whether the receiver is a tag or not.
+ *
+ * A tag is group that belongs to -[COEditingContext tagGroup].
+ */
+- (BOOL)isTag;
+/**
+ * Returns whether the receiver is a container or not.
+ */
+- (BOOL)isContainer;
+/**
+ * Returns whether the receiver is a library or not.
+ *
+ * A library is a container.
+ */
+- (BOOL)isLibrary;
+@end
---
+++ COCollection.m
@@ -0,0 +1,134 @@
+/*
+	Copyright (C) 2011 Quentin Mathe
+
+	Author:  Quentin Mathe <quentin.mathe@gmail.com>
+	Date:  December 2011
+	License:  Modified BSD  (see COPYING)
+ */
+
+#import "COCollection.h"
+
+#pragma GCC diagnostic ignored "-Wprotocol"
+
+@implementation COCollection
+
++ (void) initialize
+{
+	if (self != [COCollection class])
+		return;
+
+	[self applyTraitFromClass: [ETCollectionTrait class]];
+	[self applyTraitFromClass: [ETMutableCollectionTrait class]];
+}
+
++ (ETEntityDescription *) newEntityDescription
+{
+	ETEntityDescription *collection = [self newBasicEntityDescription];
+
+	// For subclasses that don't override -newEntityDescription, we must not add the 
+	// property descriptions that we will inherit through the parent
+	if ([[collection name] isEqual: [COCollection className]] == NO) 
+		return collection;
+
+	return collection;	
+}
+
+- (void)addObjects: (NSArray *)anArray
+{
+	for (id object in anArray)
+	{
+		[self addObject: object];
+	}
+}
+
+- (id) content
+{
+	return [self valueForProperty: @"contents"];
+}
+
+- (NSArray *) contentArray
+{
+	 // FIXME: Should return a new array, but this might break other things currently
+	return [self valueForProperty: @"contents"];
+}
+
+- (void) insertObject: (id)object atIndex: (NSUInteger)index hint: (id)hint
+{
+	assert([object editingContext] == [self editingContext]); // FIXME: change to an exception
+	if (index == ETUndeterminedIndex)
+	{
+		[self addObject: object forProperty: @"contents"];
+	}
+	else
+	{
+		[self insertObject: object atIndex: index forProperty: @"contents"];
+	}
+}
+
+- (void) removeObject: (id)object atIndex: (NSUInteger)index hint: (id)hint
+{
+	assert([object editingContext] == [self editingContext]); // FIXME: change to an exception
+	if (index == ETUndeterminedIndex)
+	{
+		[self removeObject: object forProperty: @"contents"];	
+	}
+	else
+	{
+		[self removeObject: object atIndex: index forProperty: @"contents"];
+	}
+}
+
+- (COObject *)objectForName: (NSString *)aName
+{
+	for (id object in [self content])
+	{
+		if ([[object name] isEqualToString: aName])
+		{
+			return object;
+		}
+	}
+	return nil;
+}
+
+- (NSArray *)objectsMatchingQuery: (COQuery *)aQuery
+{
+	NSMutableArray *result = [NSMutableArray array];
+
+	for (COObject *object in [self content])
+	{
+		if ([[aQuery predicate] evaluateWithObject: object])
+		{
+			[result addObject: object];
+		}
+	}
+
+	return result;
+}
+
+@end
+
+
+@implementation COObject (COCollectionTypeQuerying)
+
+- (BOOL)isGroup
+{
+	return NO;
+}
+
+- (BOOL)isTag
+{
+	return NO;
+}
+
+- (BOOL)isContainer
+{
+	return NO;
+}
+
+- (BOOL)isLibrary
+{
+	return NO;
+}
+
+@end
+
