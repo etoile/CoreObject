ed732b63ee0557f5910c30570503ece4c5f2470b
Quentin Mathé <quentin.mathe@gmail.com>
2012-10-25 12:05:41 +0000

    Added a umbrella header for CoreObject
    git-svn-id: http://svn.gna.org/svn/etoile/trunk/Etoile/Frameworks/CoreObject@7832 7b3f36aa-e6db-49c1-8b4c-66eac1997e64

---
+++ CoreObject.h
@@ -0,0 +1,32 @@
+/*
+	Copyright (C) 2012 Quentin Mathe
+
+	Author:  Quentin Mathe <quentin.mathe@gmail.com>
+	Date:  Ocstober 2012
+	License:  Modified BSD  (see COPYING)
+ */
+
+#import <CoreObject/COCollection.h>
+#import <CoreObject/COCommitTrack.h>
+#import <CoreObject/COContainer.h>
+#import <CoreObject/COCustomTrack.h>
+#import <CoreObject/COEditingContext.h>
+#import <CoreObject/COError.h>
+#import <CoreObject/COFault.h>
+#import <CoreObject/COGroup.h>
+#import <CoreObject/COHistoryTrack.h>
+#import <CoreObject/COObject.h>
+#import <CoreObject/COQuery.h>
+#import <CoreObject/CORevision.h>
+#import <CoreObject/COStore.h>
+#import <CoreObject/COSynchronizer.h>
+#import <CoreObject/COTrack.h>
+
+/* Diff Framework (the Diff API is very unstable) */
+
+#import <CoreObject/COArrayDiff.h>
+#import <CoreObject/COMergeResult.h>
+#import <CoreObject/COObjectGraphDiff.h>
+#import <CoreObject/COSequenceDiff.h>
+#import <CoreObject/COSetDiff.h>
+#import <CoreObject/COStringDiff.h>
