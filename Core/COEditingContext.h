59ffd82ad4eaed3a2f8c519a30dec916cb793e12
Eric Wasylishen <ewasylishen@gmail.com>
2013-07-18 15:14:23 +0000

    Tidy COEditingContext.h, add some TODOs
    git-svn-id: http://svn.gna.org/svn/etoile/trunk/Etoile/Frameworks/CoreObject@8676 7b3f36aa-e6db-49c1-8b4c-66eac1997e64

--- Core/COEditingContext.h
+++ Core/COEditingContext.h
@@ -4,6 +4,10 @@
 @class COPersistentRoot, COEditingContext;
 @class COSQLiteStore, CORevision, COObject, COGroup, COSmartGroup, COCommitTrack, COError, COPersistentRootInfo;
 
+// I'm skeptical that there is ever a legitimate case where code is working
+// with an id<COPersistentObjectContext> and doesn't know whether it's an
+// editing context or persistent root... but I guess it's harmless to keep for
+// now --Eric
 @protocol COPersistentObjectContext <NSObject>
 /**
  * Returns YES when the receiver is an editing context, otherwise returns NO
@@ -49,21 +53,6 @@
 	COError *_error;
 }
 
-/** @taskunit Accessing the current context */
-
-/** 
- * Returns the context that should be used when none is provided.
- *
- * Factories that create persistent instances in EtoileUI will use this method. 
- * As an example, see -[ETLayoutItemFactory compoundDocument]. 
- */
-+ (COEditingContext *)currentContext;
-/** 
- * Sets the context that should be used when none is provided.
- *
- * See also +currentContext. 
- */
-+ (void)setCurrentContext: (COEditingContext *)aCtxt;
 
 /** @taskunit Creating a new context */
 
@@ -85,13 +74,6 @@
  * As a result, the context content is not persisted.
  */
 - (id)init;
-
-/** @taskunit Debugging */
-
-/** 
- * Returns a summary that describes the uncommitted changes.
- */
-- (NSString *)description;
 
 /** @taskunit Type Querying */
 
@@ -142,11 +124,179 @@
 
 - (COPersistentRoot *)persistentRootForUUID: (ETUUID *)aUUID;
 - (COPersistentRoot *)insertNewPersistentRootWithEntityName: (NSString *)anEntityName;
+/**
+ * I'm assuming this is the mechanism for copying an embedded object and creating
+ * a new persistent root with that copy as the root object?
+ *
+ * We will need to clarify exactly how it works... presumable it does a regular
+ * metamodel driven copy, potentially copying the entire tree of children
+ * belonging to aRootObject
+ */
 - (COPersistentRoot *)insertNewPersistentRootWithRootObject: (COObject *)aRootObject;
+
+// TODO: Should probably change to take a COPersistentRoot
 - (void)deletePersistentRootForRootObject: (COObject *)aRootObject;
+
+/** @taskunit Pending Changes */
 
 @property (nonatomic, copy, readonly) NSSet *insertedPersistentRoots;
 @property (nonatomic, copy, readonly) NSSet *deletedPersistentRoots;
+
+// TODO: updatedPersistentRoots?
+// TODO: changedPersistentRoots?
+
+/**
+ * Returns whether any object has been inserted, deleted or updated since the
+ * last commit.
+ *
+ * See also -changedObjects.
+ */
+- (BOOL)hasChanges;
+
+/**
+ * Discards the uncommitted changes to reset the context to its last commit state.
+ *
+ * Persistent root insertions, deletions, and modifications (e.g., changing
+ * main branch, deleting branches, adding branches, editing branch metadata,
+ * reverting branch to a past revision) will be cancelled.
+ *
+ * All uncommitted embedded object edits in child persistent roots will be
+ * cancelled.
+ *
+ * -insertedPersistentRoots, -deletedPersistentRoots  will
+ * all return empty sets once the changes have been discarded.
+ */
+- (void)discardAllChanges;
+
+
+/** @taskunit Committing Changes */
+
+// TODO: Change to -commitWithError:
+/**
+ * Commits the current changes to the store and returns the resulting revisions.
+ *
+ * A batch commit UUID is added to the metadata of each commit to indicate that
+ * the individual persistent root commits were made as a batch.
+ *
+ * See -commitWithType:shortDescription: and -commitWithMetadata:.
+ */
+- (NSArray *)commit;
+// TODO: Change to -commitWithType:shortDescription:error:
+/**
+ * Commits the current changes to the store with some basic metadatas and 
+ * returns the resulting revisions.
+ *
+ * A commit involving multiple persistent roots is not atomic (more than a single 
+ * revision in the returned array).<br />
+ * Each returned revision results from an atomic commit on a single persistent 
+ * root.
+ *
+ * Each root object that belong to -changedObjects results in a new revision.
+ * We usually advice to commit a single root object at time to prevent multiple
+ * revisions per commit.
+ *
+ * The description will be visible at the UI level when browsing the history.
+ *
+ * See -commitWithMetadata:.
+ */
+- (NSArray *)commitWithType: (NSString *)type
+           shortDescription: (NSString *)shortDescription;
+// TODO: Remove, shouldn't we follow the regular cocoa pattern like:
+// -commitWithMetadata: (NSDictionary *) error: (NSError **)
+/** 
+ * Returns the last commit error, usually involving one or several validation 
+ * issues.
+ *
+ * When commit methods return a non-empty revision array, the error is nil.
+ */
+- (NSError *)error;
+
+/** @taskunit Private */
+
+/**
+ * This method is only exposed to be used internally by CoreObject.
+ *
+ * Instantiates, registers among the loaded persistent roots and returns the 
+ * persistent root known by the given UUID.
+ * Unlike -persistentRootForUUID:, this method doesn't access the store to 
+ * retrieve the main branch UUID, but just use the given commit track UUID.
+ *
+ * In addition, a past revision can be passed to prevent loading the persistent 
+ * root at the latest revision.
+ */
+- (COPersistentRoot *)makePersistentRootWithInfo: (COPersistentRootInfo *)info;
+/**
+ * This method is only exposed to be used internally by CoreObject.
+ */
+- (void)didFailValidationWithError: (NSError *)anError;
+/**
+ * This method is only exposed to be used internally by CoreObject.
+ *
+ * Commits the current changes to the store with the provided metadatas and
+ * returns the resulting revisions.
+ */
+- (NSArray *)commitWithMetadata: (NSDictionary *)metadata;
+/**
+ * This method is only exposed to be used internally by CoreObject.
+ *
+ * Commits some changes to the store with the provided metadatas, and returns
+ * the resulting revisions.
+ *
+ * Changes must belong to the given persistent root subset, otherwise they 
+ * won't be committed. -hasChanges can still be YES on return.
+ */
+- (NSArray *)commitWithMetadata: (NSDictionary *)metadata
+	restrictedToPersistentRoots: (NSArray *)persistentRoots;
+/**
+ * <override-never />
+ * Tells the receiver that -[COStore finishCommit] just returned.
+ *
+ * You shouldn't use this method usually. Commit methods automatically calls
+ * this method (for both COEditingContext and COPersistentRoot API).
+ *
+ * If your code uses -[COStore finishCommit] directly (e.g. in a COTrack
+ * subclass), you have to call this method explicitly.
+ */
+- (void)didCommitRevision: (CORevision *)aRevision;
+
+/** @taskunit Deprecated, to be removed */
+
+// I think we agreed these are an "anti-pattern"
+
+/**
+ * Returns the context that should be used when none is provided.
+ *
+ * Factories that create persistent instances in EtoileUI will use this method.
+ * As an example, see -[ETLayoutItemFactory compoundDocument].
+ */
++ (COEditingContext *)currentContext;
+/**
+ * Sets the context that should be used when none is provided.
+ *
+ * See also +currentContext.
+ */
++ (void)setCurrentContext: (COEditingContext *)aCtxt;
+
+// Deprecated because the 
+
+/**
+ * Returns the object identified by the UUID, by loading it to its last revision
+ * when no instance managed by the receiver is present in memory.
+ *
+ * When the UUID doesn't correspond to a persistent object, returns nil.
+ *
+ * When the object is a inner object, the last revision is the one that is tied
+ * to its root object last revision.
+ *
+ * See also -[COPersistentRoot objectWithUUID:].
+ */
+- (COObject *)objectWithUUID: (ETUUID *)uuid;
+
+@end
+
+
+@interface COEditingContext (Debugging)
+
 
 /** @taskunit Object Access and Loading */
 
@@ -172,29 +322,29 @@
 
 /** @taskunit Pending Changes */
 
-/** 
- * Returns the new objects added to the context with -insertObject: and to be 
+/**
+ * Returns the new objects added to the context with -insertObject: and to be
  * added to the store on the next commit.
  *
  * After a commit, returns an empty set.
  */
 - (NSSet *)insertedObjects;
-/** 
- * Returns the objects whose properties have been edited in the context and to 
+/**
+ * Returns the objects whose properties have been edited in the context and to
  * be updated in the store on the next commit.
  *
  * After a commit, returns an empty set.
  */
 - (NSSet *)updatedObjects;
 /**
- * Returns whether the object has been updated since the last commit. See 
+ * Returns whether the object has been updated since the last commit. See
  * -updatedObjects.
  *
  * Won't return YES if the object has just been inserted or deleted.
  */
 - (BOOL)isUpdatedObject: (COObject *)anObject;
-/** 
- * Returns the objects deleted in the context with -deleteObject: and to be 
+/**
+ * Returns the objects deleted in the context with -deleteObject: and to be
  * deleted in the store on the next commit.
  *
  * After a commit, returns an empty set.
@@ -202,128 +352,13 @@
  * Doesn't include newly inserted or deleted objects.
  */
 - (NSSet *)deletedObjects;
-/** 
- * Returns the union of the inserted, updated and deleted objects. See 
+/**
+ * Returns the union of the inserted, updated and deleted objects. See
  * -insertedObjects, -updatedObjects and -deletedObjects.
  *
  * After a commit, returns an empty set.
  */
 - (NSSet *)changedObjects;
-/**
- * Returns whether any object has been inserted, deleted or updated since the 
- * last commit.
- *
- * See also -changedObjects.
- */
-- (BOOL)hasChanges;
-/**
- * Discards the uncommitted changes to reset the context to its last commit state.
- *
- * Every object insertion or deletion is cancelled.<br /> 
- * Every updated property is reverted to its last committed value.
- *
- * -insertedObjects, -updatedObjects, -deletedObjects and -changedObjects will 
- * all return empty sets once the changes have been discarded.
- *
- * See also -discardChangesInObject:.
- */
-- (void)discardAllChanges;
 
-/** @taskunit Committing Changes */
 
-/**
- * Commits the current changes to the store and returns the resulting revisions.
- *
- * A batch commit UUID is added to the metadata of each commit to indicate that
- * the individual persistent root commits were made as a batch.
- *
- * See -commitWithType:shortDescription: and -commitWithMetadata:.
- */
-- (NSArray *)commit;
-/**
- * Commits the current changes to the store with some basic metadatas and 
- * returns the resulting revisions.
- *
- * A commit involving multiple persistent roots is not atomic (more than a single 
- * revision in the returned array).<br />
- * Each returned revision results from an atomic commit on a single persistent 
- * root.
- *
- * Each root object that belong to -changedObjects results in a new revision.
- * We usually advice to commit a single root object at time to prevent multiple
- * revisions per commit.
- *
- * The description will be visible at the UI level when browsing the history.
- *
- * See -commitWithMetadata:.
- */
-- (NSArray *)commitWithType: (NSString *)type
-           shortDescription: (NSString *)shortDescription;
-/**
- * <override-never />
- * Tells the receiver that -[COStore finishCommit] just returned.
- *
- * You shouldn't use this method usually. Commit methods automatically calls 
- * this method (for both COEditingContext and COPersistentRoot API). 
- *
- * If your code uses -[COStore finishCommit] directly (e.g. in a COTrack 
- * subclass), you have to call this method explicitly.
- */
-- (void)didCommitRevision: (CORevision *)aRevision;
-/** 
- * Returns the last commit error, usually involving one or several validation 
- * issues.
- *
- * When commit methods return a non-empty revision array, the error is nil.
- */
-- (NSError *)error;
-
-/** @taskunit Private */
-
-/**
- * This method is only exposed to be used internally by CoreObject.
- *
- * Instantiates, registers among the loaded persistent roots and returns the 
- * persistent root known by the given UUID.
- * Unlike -persistentRootForUUID:, this method doesn't access the store to 
- * retrieve the main branch UUID, but just use the given commit track UUID.
- *
- * In addition, a past revision can be passed to prevent loading the persistent 
- * root at the latest revision.
- */
-- (COPersistentRoot *)makePersistentRootWithInfo: (COPersistentRootInfo *)info;
- /**
-  * Returns the object identified by the UUID, by loading it to its last revision	 
-  * when no instance managed by the receiver is present in memory.	 
-  *	 
-  * When the UUID doesn't correspond to a persistent object, returns nil.	 
-  *	 
-  * When the object is a inner object, the last revision is the one that is tied	 
-  * to its root object last revision.	 
-  *	 
-  * See also -[COPersistentRoot objectWithUUID:].	 
-  */	 
- - (COObject *)objectWithUUID: (ETUUID *)uuid;
-/**
- * This method is only exposed to be used internally by CoreObject.
- */
-- (void)didFailValidationWithError: (NSError *)anError;
-/**
- * This method is only exposed to be used internally by CoreObject.
- *
- * Commits the current changes to the store with the provided metadatas and
- * returns the resulting revisions.
- */
-- (NSArray *)commitWithMetadata: (NSDictionary *)metadata;
-/**
- * This method is only exposed to be used internally by CoreObject.
- *
- * Commits some changes to the store with the provided metadatas, and returns 
- * the resulting revisions.
- *
- * Changes must belong to the given persistent root subset, otherwise they 
- * won't be committed. -hasChanges can still be YES on return.
- */
-- (NSArray *)commitWithMetadata: (NSDictionary *)metadata
-	restrictedToPersistentRoots: (NSArray *)persistentRoots;
 @end
