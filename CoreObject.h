d2519817853e802fa3b79f5184568791e402a0fc
Quentin Mathé <quentin.mathe@gmail.com>
2013-07-25 10:36:01 +0000

    Fully updated CoreObject.h
    git-svn-id: http://svn.gna.org/svn/etoile/trunk/Etoile/Frameworks/CoreObject@8743 7b3f36aa-e6db-49c1-8b4c-66eac1997e64

--- CoreObject.h
+++ CoreObject.h
@@ -6,32 +6,59 @@
 	License:  Modified BSD  (see COPYING)
  */
 
-#import <CoreObject/COBookmark.h>
-#import <CoreObject/COCollection.h>
-#import <CoreObject/COCommitDescriptor.h>
-#import <CoreObject/COBranch.h>
-#import <CoreObject/COContainer.h>
-#import <CoreObject/COCustomTrack.h>
+/* Core */
+
+#import <CoreObject/COCopier.h>
 #import <CoreObject/COEditingContext.h>
-#import <CoreObject/COError.h>
-#import <CoreObject/COGroup.h>
-#import <CoreObject/COHistoryTrack.h>
-#import <CoreObject/COLibrary.h>
 #import <CoreObject/COObject.h>
+#import <CoreObject/COObjectGraphContext.h>
 #import <CoreObject/COPersistentRoot.h>
 #import <CoreObject/COQuery.h>
 #import <CoreObject/CORevision.h>
-#import <CoreObject/COStore.h>
-#import <CoreObject/COSQLStore.h>
+#import <CoreObject/COSerialization.h>
+#import <CoreObject/COSQLiteStore.h>
 #import <CoreObject/COSynchronizer.h>
+#import <CoreObject/NSObject+CoreObject.h>
+
+/* Model */
+
+#import <CoreObject/COBookmark.h>
+#import <CoreObject/COCollection.h>
+#import <CoreObject/COContainer.h>
+#import <CoreObject/CODictionary.h>
+#import <CoreObject/COGroup.h>
+#import <CoreObject/COLibrary.h>
 #import <CoreObject/COTag.h>
+
+/* Tracks */
+
+#import <CoreObject/COBranch.h>
+#import <CoreObject/COCustomTrack.h>
+#import <CoreObject/COHistoryTrack.h>
 #import <CoreObject/COTrack.h>
 
 /* Diff Framework (the Diff API is very unstable) */
 
 #import <CoreObject/COArrayDiff.h>
-#import <CoreObject/COMergeResult.h>
-#import <CoreObject/COObjectGraphDiff.h>
-#import <CoreObject/COSequenceDiff.h>
-#import <CoreObject/COSetDiff.h>
-#import <CoreObject/COStringDiff.h>
+#import <CoreObject/COItemGraphDiff.h>
+#import <CoreObject/COSubtreeEdits.h>
+
+/* Storage Data Model */
+
+#import <CoreObject/COItem.h>
+#import <CoreObject/COItemGraph.h>
+#import <CoreObject/COType.h>
+
+/* Store */
+
+#import <CoreObject/CORevisionID.h>
+#import <CoreObject/CORevisionInfo.h>
+#import <CoreObject/COSearchResult.h>
+#import <CoreObject/COSQLiteStore.h>
+#import <CoreObject/COSQLiteStore+Attachments.h>
+#import <CoreObject/COSQLiteStorePersistentRootBackingStore.h>
+
+/* Utilities */
+
+#import <CoreObject/COCommitDescriptor.h>
+#import <CoreObject/COError.h>
