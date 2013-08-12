9c249178f02d415a901709833752439e7d9487e6
Eric Wasylishen <ewasylishen@gmail.com>
2013-08-12 21:50:09 +0000

    Enable TestPerformance.m and TestUtilities.m
    git-svn-id: http://svn.gna.org/svn/etoile/trunk/Etoile/Frameworks/CoreObject@8870 7b3f36aa-e6db-49c1-8b4c-66eac1997e64

--- Tests/TestPerformance.m
+++ Tests/TestPerformance.m
@@ -4,33 +4,33 @@
 #import "TestCommon.h"
 #import "COContainer.h"
 
-@interface TestPerformance : NSObject <UKTest>
+@interface TestPerformance : TestCommon <UKTest>
 @end
 
 @implementation TestPerformance
 
-#if 0
 - (void)testManyObjects
 {
-	UKFalse([ctx hasChanges]);
+    COPersistentRoot *persistentRoot = [ctx insertNewPersistentRootWithEntityName: @"Anonymous.OutlineItem"];
+	COObjectGraphContext *graph = [[persistentRoot editingBranch] objectGraphContext];
 	
 	NSLog(@"Starting performance test");
 	
-	COContainer *root = [ctx insertObjectWithEntityName: @"Anonymous.OutlineItem"];
+	COContainer *root = (COContainer *)[graph rootObject];
 	for (int i=0; i<10; i++)
 	{
 		NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
-		COContainer *level1 = [ctx insertObjectWithEntityName: @"Anonymous.OutlineItem"];
+		COContainer *level1 = [graph insertObjectWithEntityName: @"Anonymous.OutlineItem"];
 		[level1 setValue: [NSString stringWithFormat: @"%d", i] forProperty: @"label"];
 		[root addObject: level1];
 		for (int j=0; j<10; j++)
 		{
-			COContainer *level2 = [ctx insertObjectWithEntityName: @"Anonymous.OutlineItem"];
+			COContainer *level2 = [graph insertObjectWithEntityName: @"Anonymous.OutlineItem"];
 			[level2 setValue: [NSString stringWithFormat: @"%d.%d", i, j] forProperty: @"label"];
 			[level1 addObject: level2];
 			for (int k=0; k<10; k++)
 			{
-				COContainer *level3 = [ctx insertObjectWithEntityName: @"Anonymous.OutlineItem"];
+				COContainer *level3 = [graph insertObjectWithEntityName: @"Anonymous.OutlineItem"];
 				[level3 setValue: [NSString stringWithFormat: @"%d.%d.%d", i, j, k] forProperty: @"label"];
 				[level2 addObject: level3];
 			}
@@ -46,6 +46,5 @@
 
 	UKPass();
 }
-#endif
 
 @end
