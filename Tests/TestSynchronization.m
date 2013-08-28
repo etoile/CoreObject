14056ec8bd13cf7e0b546c05e9c73f1f25b2d4c5
Eric Wasylishen <ewasylishen@gmail.com>
2013-08-28 23:47:45 +0000

    work on TestSynchronization
    git-svn-id: http://svn.gna.org/svn/etoile/trunk/Etoile/Frameworks/CoreObject@8963 7b3f36aa-e6db-49c1-8b4c-66eac1997e64

--- Tests/TestSynchronization.m
+++ Tests/TestSynchronization.m
@@ -8,6 +8,31 @@
 
 @implementation TestSynchronization
 
+static ETUUID *persistentRootUUID;
+static ETUUID *rootItemUUID;
+static ETUUID *branchAUUID;
+static ETUUID *branchBUUID;
+
++ (void) initialize
+{
+    if (self == [TestSynchronization class])
+    {
+        rootItemUUID = [[ETUUID alloc] init];
+        persistentRootUUID = [[ETUUID alloc] init];
+        branchAUUID = [[ETUUID alloc] init];
+        branchBUUID = [[ETUUID alloc] init];        
+    }
+}
+
+- (COItemGraph *) itemGraphWithLabel: (NSString *)aLabel
+{
+    COMutableItem *child = [[[COMutableItem alloc] initWithUUID: rootItemUUID] autorelease];
+    [child setValue: aLabel
+       forAttribute: @"label"
+               type: kCOTypeString];
+    return [COItemGraph itemGraphWithItemsRootFirst: @[child]];
+}
+
 - (id)init
 {
 	SUPERINIT;
@@ -21,6 +46,13 @@
 
 - (void)testBasic
 {
+    COPersistentRootInfo *info = [store createPersistentRootWithInitialItemGraph: [self itemGraphWithLabel: @"1"]
+                                                                            UUID: persistentRootUUID
+                                                                      branchUUID: branchAUUID
+                                                                revisionMetadata: nil
+                                                                           error: NULL];
+    UKNotNil(info);
+    
     
 }
 
