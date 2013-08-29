04911d6d45ec06daeb6a2f3f276cd47a848e3163
Eric Wasylishen <ewasylishen@gmail.com>
2013-08-29 23:38:20 +0000

    Add a disabled test for -mutableArrayValueForKey: (which isn't yet implemented on COObject)
    git-svn-id: http://svn.gna.org/svn/etoile/trunk/Etoile/Frameworks/CoreObject@8977 7b3f36aa-e6db-49c1-8b4c-66eac1997e64

--- Tests/TestCOObjectSynthesizedAccessors.m
+++ Tests/TestCOObjectSynthesizedAccessors.m
@@ -1,14 +1,29 @@
 #import "TestCommon.h"
 
 @interface TestCOObjectSynthesizedAccessors : TestCommon <UKTest>
+{
+    COPersistentRoot *persistentRoot;
+}
 @end
 
 @implementation TestCOObjectSynthesizedAccessors
 
-- (void) testBasic
+- (id) init
 {
-    COPersistentRoot *persistentRoot = [ctx insertNewPersistentRootWithEntityName: @"Anonymous.OutlineItem"];
+    SUPERINIT;
+    ASSIGN(persistentRoot, [ctx insertNewPersistentRootWithEntityName: @"Anonymous.OutlineItem"]);
     
+    return self;
+}
+
+- (void)dealloc
+{
+    [persistentRoot release];
+    [super dealloc];
+}
+
+- (void) testAttributeGetterAndSetter
+{
     OutlineItem *item = [persistentRoot rootObject];
     UKObjectKindOf(item, OutlineItem);
     
@@ -24,5 +39,21 @@
     [item setContents: A(child1, child2)];
     UKObjectsEqual(A(child1, child2), [item contents]);
 }
+
+#if 0
+- (void) testMutableProxy
+{
+    OutlineItem *item = [persistentRoot rootObject];
+
+    OutlineItem *child1 = [[persistentRoot objectGraphContext] insertObjectWithEntityName: @"Anonymous.OutlineItem"];
+
+    // FIXME: Change to mutableOrderedSetValueForKey
+    [[item mutableArrayValueForKey: @"children"] addObject: child1];
+    UKObjectsEqual(@[child1], [item contents]);
+
+    [[item mutableArrayValueForKey: @"children"] removeObject: child1];
+    UKObjectsEqual(@[], [item contents]);
+}
+#endif
 
 @end
