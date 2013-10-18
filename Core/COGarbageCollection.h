440f7db66c02e59f387e6b3881311944f66b07a8
Eric Wasylishen <ewasylishen@gmail.com>
2013-10-18 20:40:12 +0000

    Clean up garbage collection code. Add TERMINOLOGY file (empty so far)
    git-svn-id: http://svn.gna.org/svn/etoile/trunk/Etoile/Frameworks/CoreObject@9339 7b3f36aa-e6db-49c1-8b4c-66eac1997e64

--- Core/COGarbageCollection.h
+++ Core/COGarbageCollection.h
@@ -13,18 +13,4 @@
 
 @interface COObject (COGarbageCollection)
 
-/** @taskunit Contained Objects based on the Metamodel */
-
-/**
- * This method is only exposed to be used internally by CoreObject.
- *
- * Returns an array containing all COObjects "strongly contained" by this one.
- * This means objects which are values for "composite" properties.
- */
-- (NSArray *)allStronglyContainedObjects;
-/**
- * This method is only exposed to be used internally by CoreObject.
- */
-- (NSArray*)embeddedOrReferencedObjects;
-
 @end
--- Core/COObjectGraphContext.m
+++ Core/COObjectGraphContext.m
@@ -17,6 +17,7 @@
 #import "COPersistentRoot.h"
 #import "COBranch.h"
 #import "COItem.h"
+#import "CODictionary.h"
 
 NSString * const COObjectGraphContextObjectsDidChangeNotification = @"COObjectGraphContextObjectsDidChangeNotification";
 
@@ -475,6 +476,59 @@ NSString * const COObjectGraphContextObjectsDidChangeNotification = @"COObjectGr
     anObject = nil;
 }
 
+/**
+ * Given a COObject, returns an array of all of the COObjects directly reachable
+ * from that COObject.
+ */
+static NSArray *DirectlyReachableObjectsFromObject(COObject *anObject)
+{
+	NSMutableArray *result = [NSMutableArray array];
+	for (ETPropertyDescription *propDesc in [[anObject entityDescription] allPropertyDescriptions])
+	{
+		// FIXME: This should be uncommented, but it causes test failures.
+//		if (![propDesc isPersistent])
+//		{
+//			continue;
+//		}
+		
+		NSString *propertyName = [propDesc name];
+		id value = [anObject valueForKey: propertyName];
+        
+        if ([propDesc isMultivalued])
+        {
+			if ([propDesc isKeyed])
+			{
+				assert([value isKindOfClass: [CODictionary class]] || [value isKindOfClass: [NSDictionary class]]);
+			}
+			else
+			{
+				assert([value isKindOfClass: [NSArray class]] || [value isKindOfClass: [NSSet class]]);
+				
+			}
+			
+			/* We use -objectEnumerator, because subvalue can be a  CODictionary
+			 or a NSDictionary (if a getter exists to expose the CODictionary
+			 as a NSDictionary for UI editing) */
+            for (id subvalue in [value objectEnumerator])
+            {
+                if ([subvalue isKindOfClass: [COObject class]])
+                {
+                    [result addObject: subvalue];
+                }
+            }
+        }
+        else
+        {
+            if ([value isKindOfClass: [COObject class]])
+            {
+                [result addObject: value];
+            }
+            // Ignore non-COObject objects
+        }
+	}
+	return result;
+}
+
 static void FindReachableObjectsFromObject(COObject *anObject, NSMutableSet *collectedUUIDSet)
 {
     ETUUID *uuid = [anObject UUID];
@@ -485,7 +539,7 @@ static void FindReachableObjectsFromObject(COObject *anObject, NSMutableSet *col
     [collectedUUIDSet addObject: uuid];
     
     // Call recursively on all composite and referenced objects
-    for (COObject *obj in [anObject embeddedOrReferencedObjects])
+    for (COObject *obj in DirectlyReachableObjectsFromObject(anObject))
     {
         FindReachableObjectsFromObject(obj, collectedUUIDSet);
     }
