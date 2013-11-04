1407435e87fdd53c99dbdffc34aabec0f1220e25
Eric Wasylishen <ewasylishen@gmail.com>
2013-11-04 08:04:10 +0000

    Store/COSQLiteStore+Debugging.m: enhance graph output
    git-svn-id: http://svn.gna.org/svn/etoile/trunk/Etoile/Frameworks/CoreObject@9517 7b3f36aa-e6db-49c1-8b4c-66eac1997e64

--- Store/COSQLiteStore+Debugging.m
+++ Store/COSQLiteStore+Debugging.m
@@ -2,34 +2,41 @@
 #import "COSQLiteStore+Private.h"
 #import "COSQLiteStorePersistentRootBackingStore.h"
 #import "CORevisionInfo.h"
+#import "COPersistentRootInfo.h"
+#import "COBranchInfo.h"
 
 @implementation COSQLiteStore (Debugging)
 
-- (NSString *) dotStringForRevisionUUID: (ETUUID *)aUUID
+- (NSString *) dotNameForRevisionUUID: (ETUUID *)aUUID
 {
 	NSString *str = [aUUID stringValue];
-	str = [str stringByReplacingOccurrencesOfString: @"-.*"
-										 withString: @""
-											options: NSRegularExpressionSearch
-											  range: NSMakeRange(0, [str length])];
+	str = [str substringToIndex: 4]; // HACK: Trim the UUID after the 4th char
 	
 	return [@"r" stringByAppendingString: str];
+}
+
+- (NSString *) dotNameForBranchUUID: (ETUUID *)aUUID
+{
+	NSString *str = [aUUID stringValue];
+	str = [str substringToIndex: 4]; // HACK: Trim the UUID after the 4th char
+	
+	return [@"branch_" stringByAppendingString: str];
 }
 
 - (void) writeDotNodeForRevisionInfo: (CORevisionInfo *)revInfo toString: (NSMutableString *)dest
 {
 	if (revInfo.mergeParentRevisionUUID != nil)
 	{
-		[dest appendFormat: @"\t%@ -> %@;\n", [self dotStringForRevisionUUID: revInfo.mergeParentRevisionUUID], [self dotStringForRevisionUUID: revInfo.revisionUUID]];
-		[dest appendFormat: @"\t%@ -> %@;\n", [self dotStringForRevisionUUID: revInfo.parentRevisionUUID], [self dotStringForRevisionUUID: revInfo.revisionUUID]];
+		[dest appendFormat: @" %@ -> %@;\n", [self dotNameForRevisionUUID: revInfo.mergeParentRevisionUUID], [self dotNameForRevisionUUID: revInfo.revisionUUID]];
+		[dest appendFormat: @" %@ -> %@;\n", [self dotNameForRevisionUUID: revInfo.parentRevisionUUID], [self dotNameForRevisionUUID: revInfo.revisionUUID]];
 	}
 	else if (revInfo.parentRevisionUUID != nil)
 	{
-		[dest appendFormat: @"\t%@ -> %@;\n", [self dotStringForRevisionUUID: revInfo.parentRevisionUUID], [self dotStringForRevisionUUID: revInfo.revisionUUID]];
+		[dest appendFormat: @" %@ -> %@;\n", [self dotNameForRevisionUUID: revInfo.parentRevisionUUID], [self dotNameForRevisionUUID: revInfo.revisionUUID]];
 	}
 	else
 	{
-		[dest appendFormat: @"\t%@;\n", [self dotStringForRevisionUUID: revInfo.revisionUUID]];
+		[dest appendFormat: @" %@;\n", [self dotNameForRevisionUUID: revInfo.revisionUUID]];
 	}
 }
 
@@ -37,6 +44,8 @@
 {
 	NSMutableString *result = [NSMutableString string];
     
+	COPersistentRootInfo *info = [self persistentRootInfoForUUID: aPersistentRoot];
+	
     assert(dispatch_get_current_queue() != queue_);
     
     dispatch_sync(queue_, ^(){
@@ -44,6 +53,8 @@
 																				createIfNotPresent: YES];
 		
 		[result appendString: @"digraph G {\n"];
+		
+		// Add revisions
 		
 		NSIndexSet *revidsUsed = [backing revidsUsedRange];
 		for (NSUInteger i = [revidsUsed firstIndex]; i != NSNotFound; i = [revidsUsed indexGreaterThanIndex: i])
@@ -56,6 +67,48 @@
 			}
 		}
 		
+		// Add branches
+		
+		for (ETUUID *branchUUID in info.branchUUIDs)
+		{
+			COBranchInfo *branchInfo = [info branchInfoForUUID:branchUUID];
+			
+			if ([branchInfo.headRevisionUUID isEqual: branchInfo.currentRevisionUUID])
+			{
+				[result appendFormat: @" %@ -> %@ [style=dotted];\n",
+				 [self dotNameForBranchUUID: branchInfo.UUID],
+				 [self dotNameForRevisionUUID: branchInfo.headRevisionUUID]];
+			}
+			else
+			{
+				[result appendFormat: @" %@ -> %@ [style=dotted,label=\"head\"];\n",
+				 [self dotNameForBranchUUID: branchInfo.UUID],
+				 [self dotNameForRevisionUUID: branchInfo.headRevisionUUID]];
+
+				[result appendFormat: @" %@ -> %@ [style=dotted,label=\"current\"];\n",
+				 [self dotNameForBranchUUID: branchInfo.UUID],
+				 [self dotNameForRevisionUUID: branchInfo.currentRevisionUUID]];
+			}
+			
+			if (branchInfo.metadata != nil)
+			{
+				[result appendFormat: @" %@ -> %@_metadata [style=dotted,label=\"metadata\"];\n",
+				 [self dotNameForBranchUUID: branchInfo.UUID],
+				 [self dotNameForBranchUUID: branchInfo.UUID]];
+
+				NSString *escapedMetadata = [[NSString alloc] initWithData: [NSJSONSerialization dataWithJSONObject: branchInfo.metadata options: 0 error: NULL]
+																  encoding: NSUTF8StringEncoding];
+				// FIXME: escape escapedMetadata
+				
+				[result appendFormat: @" %@_metadata [shape=box,color=red,label=<%@>];\n",
+				 [self dotNameForBranchUUID: branchInfo.UUID],
+				 escapedMetadata];
+			}
+			
+			[result appendFormat: @" %@ [shape=box];\n",
+			 [self dotNameForBranchUUID: branchInfo.UUID]];
+		}
+		
 		[result appendString: @"}\n"];
 
     });
@@ -65,12 +118,15 @@
 
 - (void) showGraphForPersistentRootUUID: (ETUUID *)aUUID
 {
-	NSString *basePath = [NSTemporaryDirectory() stringByAppendingPathComponent: [aUUID stringValue]];
+	NSString *basePath = [NSString stringWithFormat: @"%@-%d",
+						  [NSTemporaryDirectory() stringByAppendingPathComponent: [aUUID stringValue]],
+						  rand()];
+	
 	NSString *dotGraphPath = [basePath stringByAppendingPathExtension: @"gv"];
-	NSString *psPath = [basePath stringByAppendingPathExtension: @"ps"];
+	NSString *pdfPath = [basePath stringByAppendingPathExtension: @"pdf"];
 	[[self dotGraphForPersistentRootUUID: aUUID] writeToFile: dotGraphPath atomically: YES encoding: NSUTF8StringEncoding error: NULL];
 	
-	system([[NSString stringWithFormat: @"dot -Tps %@ -o %@ && open %@", dotGraphPath, psPath, psPath] UTF8String]);
+	system([[NSString stringWithFormat: @"dot -Tpdf %@ -o %@ && open %@", dotGraphPath, pdfPath, pdfPath] UTF8String]);
 }
 
 @end
