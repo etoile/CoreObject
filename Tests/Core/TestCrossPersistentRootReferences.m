b7141858ec814f94997e6f15c9db28131f1fa829
Eric Wasylishen <ewasylishen@gmail.com>
2013-11-22 21:01:05 +0000

    TestCrossPersistentRootReferences: move a disabled test case to Scraps
    git-svn-id: http://svn.gna.org/svn/etoile/trunk/Etoile/Frameworks/CoreObject@9661 7b3f36aa-e6db-49c1-8b4c-66eac1997e64

--- Tests/Core/TestCrossPersistentRootReferences.m
+++ Tests/Core/TestCrossPersistentRootReferences.m
@@ -76,54 +76,6 @@
 	 }];
 }
 
-#if 0
-
-- (void) testAsyncFaulting
-{
-    // library1 <<persistent root>>
-	//  |
-	//  \--photo1 // cross-persistent-root link, default branch
-	//
-	// photo1 <<persistent root>>
-	//  |
-	//  \--child1 (pretend there are a lot of child objects)
-    
-    
-    // 1. Set it up in memory
-    
-    COPersistentRoot *photo1 = [ctx insertNewPersistentRootWithEntityName: @"Anonymous.OutlineItem"];
-    COObject *child1 = [photo1 insertObjectWithEntityName: @"Anonymous.OutlineItem"];
-
-    [[photo1 rootObject] setValue: @"photo1" forProperty: @"label"];
-    [[photo1 rootObject] insertObject: child1 atIndex: ETUndeterminedIndex hint: nil forProperty: @"contents"];
-    
-    COPersistentRoot *persistentRoot = [ctx insertNewPersistentRootWithEntityName: @"Anonymous.OutlineItem"];
-    [[persistentRoot rootObject] insertObject: [photo1 rootObject] atIndex: ETUndeterminedIndex hint:nil forProperty: @"contents"];
-    
-    [ctx commit];
-    
-    // 2. Read it into another context
-    
-    {
-        COEditingContext *ctx2 = [[COEditingContext alloc] initWithStore: store];
-        COPersistentRoot *persistentRoot2 = [ctx2 persistentRootForUUID: [persistentRoot persistentRootUUID]];
-        COPersistentRoot *photo1ctx2 = [[[[persistentRoot2 rootObject] valueForKey: @"contents"] objectAtIndex: 0] persistentRoot];
-        
-        // This should be a fault
-        COObject *photo1ctx2RootObject = [photo1ctx2 rootObject];
-        
-        // The relationship has not loaded yet.
-        UKObjectsEqual([NSArray array], [photo1ctx2RootObject valueForKey: @"contents"]);
-        
-        // Now, suppose that triggers loading.
-        // Should we automatically notify a delegate?
-        
-        // FIXME: Check for the loaded contents
-    }
-}
-
-#endif
-
 /*
  Verifies that when you have references to:
  
