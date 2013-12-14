13e0edca4bcb272b303f4796cfbc3de4eda85b1b
Eric Wasylishen <ewasylishen@gmail.com>
2013-12-14 03:57:58 +0000

    Benchmark/: Add MIT license headers
    git-svn-id: http://svn.gna.org/svn/etoile/trunk/Etoile/Frameworks/CoreObject@9871 7b3f36aa-e6db-49c1-8b4c-66eac1997e64

--- Benchmark/TestObjectGraphPerformance.m
+++ Benchmark/TestObjectGraphPerformance.m
@@ -1,3 +1,11 @@
+/*
+	Copyright (C) 2010 Eric Wasylishen
+
+	Author:  Eric Wasylishen <ewasylishen@gmail.com>
+	Date:  November 2010
+	License:  MIT  (see COPYING)
+ */
+
 #import <Foundation/Foundation.h>
 #import <UnitKit/UnitKit.h>
 #import "COEditingContext.h"
