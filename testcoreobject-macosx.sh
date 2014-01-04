a461dad42eaf9c4e0103e2c6d9154c66ca7d75a2
Eric Wasylishen <ewasylishen@gmail.com>
2014-01-04 02:52:02 +0000

    testcoreobject-macosx.sh: Also run BenchmarkCoreObject and BasicPersistence targets
    git-svn-id: http://svn.gna.org/svn/etoile/trunk/Etoile/Frameworks/CoreObject@9997 7b3f36aa-e6db-49c1-8b4c-66eac1997e64

--- testcoreobject-macosx.sh
+++ testcoreobject-macosx.sh
@@ -1,2 +1,32 @@
 #!/bin/bash
-TESTCOREOBJECT_AUTORUN=1 xcodebuild -project CoreObject.xcodeproj -scheme TestCoreObject
+
+# This is used in the "Run Script" phase of each of the targets we build,
+# if it is set to 1, the resulting binary is run as part of the xcodebuild
+export TESTCOREOBJECT_AUTORUN=1 
+
+xcodebuild -project CoreObject.xcodeproj -scheme TestCoreObject
+teststatus=$?
+
+xcodebuild -project CoreObject.xcodeproj -scheme BenchmarkCoreObject
+benchmarkstatus=$?
+
+xcodebuild -project CoreObject.xcodeproj -scheme BasicPersistence
+samplestatus=$?
+
+# printstatus 'message' status
+function printstatus {
+  if [[ $2 == 0 ]]; then
+    echo "(PASS) $1"
+  else
+    echo "(FAIL) $1"
+  fi
+}
+
+echo "CoreObject Tests Summary"
+echo "========================"
+printstatus TestCoreObject $teststatus
+printstatus BenchmarkCoreObject $benchmarkstatus
+printstatus BasicPersistence $samplestatus
+
+exitstatus=$(( teststatus || benchmarkstatus || samplestatus ))
+exit $exitstatus
