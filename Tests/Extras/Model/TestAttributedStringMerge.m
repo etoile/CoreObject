34ca1bc94815bcc27642264fef2540332c64f63d
Eric Wasylishen <ewasylishen@gmail.com>
2014-01-09 02:24:45 +0000

    TestAttributedStringMerge: Add test case for 'hello' merge that failed in ProjectDemo today (it's reproducible too). The raw test case here passes though, not sure what causes the difference
    git-svn-id: http://svn.gna.org/svn/etoile/trunk/Etoile/Frameworks/CoreObject@10022 7b3f36aa-e6db-49c1-8b4c-66eac1997e64

--- Tests/Extras/Model/TestAttributedStringMerge.m
+++ Tests/Extras/Model/TestAttributedStringMerge.m
@@ -83,4 +83,75 @@
 	UKObjectsEqual(A(S(),  S(@"b"), S(@"b", @"i"), S(@"i")), [[ctx1 rootObject] valueForKeyPath: @"chunks.attributes.htmlCode"]);
 }
 
+- (void) testMergeOverlappingAttributeAdditions2
+{
+	/*
+	 ctx1:
+	 
+	 "Hello"
+	 
+	 */
+	
+	COObjectGraphContext *ctx1 = [self makeAttributedString];
+	[self appendString: @"Hello" htmlCode: nil toAttributedString: [ctx1 rootObject]];
+	
+	
+	/*
+	 ctx2:
+	 
+	 "Hello"
+	  ^^^^
+	 bold
+	 
+	 */
+	
+	COObjectGraphContext *ctx2 = [COObjectGraphContext new];
+	[ctx2 setItemGraph: ctx1];
+	[self clearAttributedString: [ctx2 rootObject]];
+	[self appendString: @"Hell" htmlCode: @"b" toAttributedString: [ctx2 rootObject]];
+	[self appendString: @"o" htmlCode: nil toAttributedString: [ctx2 rootObject]];
+	
+	/*
+	 ctx3:
+	 
+	 "Hello"
+		^^^
+	 underline
+	 
+	 */
+	
+	
+	COObjectGraphContext *ctx3 = [COObjectGraphContext new];
+	[ctx3 setItemGraph: ctx1];
+	[self clearAttributedString: [ctx3 rootObject]];
+	
+	[self appendString: @"He" htmlCode: nil toAttributedString: [ctx3 rootObject]];
+	[self appendString: @"llo" htmlCode: @"u" toAttributedString: [ctx3 rootObject]];
+	
+	COAttributedStringDiff *diff12 = [[COAttributedStringDiff alloc] initWithFirstAttributedString: [ctx1 rootObject]
+																			secondAttributedString: [ctx2 rootObject]
+																							source: @"diff12"];
+	
+    COAttributedStringDiff *diff13 = [[COAttributedStringDiff alloc] initWithFirstAttributedString: [ctx1 rootObject]
+																			secondAttributedString: [ctx3 rootObject]
+																							source: @"diff13"];
+	
+	[diff13 addOperationsFromDiff: diff12];
+	[diff13 applyToAttributedString: [ctx1 rootObject]];
+	
+	/*
+	 ctxExpected:
+	 
+	 "Hello"
+	  ^^^^
+	 bold
+	    ^^^
+		underline
+	 
+	 */
+	
+	UKObjectsEqual(A(@"He",    @"ll",         @"o"), [[ctx1 rootObject] valueForKeyPath: @"chunks.text"]);
+	UKObjectsEqual(A(S(@"b"),  S(@"b", @"u"), S(@"u")), [[ctx1 rootObject] valueForKeyPath: @"chunks.attributes.htmlCode"]);
+}
+
 @end
