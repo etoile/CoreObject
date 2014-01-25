f443b9cdbd759ac7ca0c798a1cac17b32e1c0a3c
Eric Wasylishen <ewasylishen@gmail.com>
2014-01-25 04:13:26 +0000

    main.m: Restructure so it's easier to check for leaks. Add a comment about known COSQLiteStore leaks
    git-svn-id: http://svn.gna.org/svn/etoile/trunk/Etoile/Frameworks/CoreObject@10138 7b3f36aa-e6db-49c1-8b4c-66eac1997e64

--- Tests/main.m
+++ Tests/main.m
@@ -13,6 +13,8 @@
 
 int main (int argc, const char *argv[])
 {
+	int status = 0;
+	
     @autoreleasepool {
 		UKRunner *runner = [UKRunner new];
 
@@ -28,11 +30,29 @@ int main (int argc, const char *argv[])
 
 		if ([handler exceptionsReported] > 0 || [handler testsFailed] > 0)
 		{
-			return 1;
+			status = 1;
 		}
-		else
-		{
-			return 0;
-		}
-    }
+	}
+
+	// Run a runloop so we handle any outstanding notifications, so
+	// we can check for leaks afterwards.
+	
+	@autoreleasepool
+	{
+		NSRunLoop *runLoop = [NSRunLoop currentRunLoop];
+		[runLoop runUntilDate: [NSDate dateWithTimeIntervalSinceNow: 0.1]];
+	}
+
+	// TODO: Maybe count up the number of leaked COSQLiteStore instances at this
+	// point.
+	//
+	// As of 2014-01-24, there are 3 leaked stores:
+	//
+	//  - In TestSynchronizer, -testBasicServerRevert and -testBasicClientRevert each leak a store.
+	//    (I don't understand why, but they're not so serious because
+	//     they only happen when throwing an exception in response to incorrect API usage.)
+	//
+	//  - +[COUndoStackStore defaultStore] intentionally leaks a database connection.
+
+	return status;
 }
