f965a447f8b4a7c1dbca648ed4cd155e44a9bbea
Eric Wasylishen <ewasylishen@gmail.com>
2014-01-31 20:58:24 +0000

    Refactor TestAttributedStringMerge
    git-svn-id: http://svn.gna.org/svn/etoile/trunk/Etoile/Frameworks/CoreObject@10196 7b3f36aa-e6db-49c1-8b4c-66eac1997e64

--- Tests/Extras/Model/TestAttributedStringMerge.m
+++ Tests/Extras/Model/TestAttributedStringMerge.m
@@ -12,208 +12,135 @@
 
 @implementation TestAttributedStringMerge
 
-- (void) testMergeOverlappingAttributeAdditions
+- (void) appendHTMLString: (NSString *)html toAttributedString: (COAttributedString *)dest
 {
-	/*
-	 ctx1:
-	 
-	 "abc"
-	 	 
-	 */
+	NSUInteger len = [html length];
 	
-	COObjectGraphContext *ctx1 = [self makeAttributedString];
-	[self appendString: @"abc" htmlCode: nil toAttributedString: [ctx1 rootObject]];
+	NSMutableSet *attributes = [NSMutableSet new];
+	BOOL inAngleBrackets = NO;
+	BOOL isRemoving = NO;
+	NSString *htmlCode = @"";
+	
+	for (NSUInteger i = 0; i < len; i++)
+	{
+		NSString *character = [html substringWithRange: NSMakeRange(i, 1)];
+		if (inAngleBrackets)
+		{
+			if ([character isEqualToString: @"\\"])
+			{
+				isRemoving = YES;
+			}
+			else if ([character isEqualToString: @">"])
+			{
+				htmlCode = [htmlCode lowercaseString];
+				if (isRemoving)
+				{
+					[attributes removeObject: htmlCode];
+				}
+				else
+				{
+					[attributes addObject: htmlCode];
+				}
+				
+				inAngleBrackets = NO;
+				isRemoving = NO;
+				htmlCode = @"";
+			}
+			else
+			{
+				htmlCode = [htmlCode stringByAppendingString: character];
+			}
+		}
+		else
+		{
+			if ([character isEqualToString: @"<"])
+			{
+				inAngleBrackets = YES;
+			}
+			else
+			{
+				[self appendString: character htmlCodes: [attributes allObjects] toAttributedString: dest];
+			}
+		}
+	}
+}
 
-	
-	/*
-	 ctx2:
-	 
-	 "abc"
-	  ^^
-	  bold
-	 
-	 */
-	
+- (void) checkMergingBase: (NSString *)base
+			  withBranchA: (NSString *)branchA
+			  withBranchB: (NSString *)branchB
+					gives: (NSString *)result
+{
+	COObjectGraphContext *ctx1 = [self makeAttributedString];
+	[self appendHTMLString: base toAttributedString: [ctx1 rootObject]];
+
 	COObjectGraphContext *ctx2 = [COObjectGraphContext new];
 	[ctx2 setItemGraph: ctx1];
 	[self clearAttributedString: [ctx2 rootObject]];
-	[self appendString: @"ab" htmlCode: @"b" toAttributedString: [ctx2 rootObject]];
-	[self appendString: @"c" htmlCode: nil toAttributedString: [ctx2 rootObject]];
-	
-	/*
-	 ctx3:
-	 
-	 "dabc"
-	    ^^
-	    italic
-	 
-	 */
-
+	[self appendHTMLString: branchA toAttributedString: [ctx2 rootObject]];
 	
 	COObjectGraphContext *ctx3 = [COObjectGraphContext new];
 	[ctx3 setItemGraph: ctx1];
 	[self clearAttributedString: [ctx3 rootObject]];
-
-	[self appendString: @"da" htmlCode: nil toAttributedString: [ctx3 rootObject]];
-	[self appendString: @"bc" htmlCode: @"i" toAttributedString: [ctx3 rootObject]];
+	[self appendHTMLString: branchB toAttributedString: [ctx3 rootObject]];
 	
-	COAttributedStringDiff *diff12 = [[COAttributedStringDiff alloc] initWithFirstAttributedString: [ctx1 rootObject]
+	COAttributedStringDiff *diffA = [[COAttributedStringDiff alloc] initWithFirstAttributedString: [ctx1 rootObject]
 																			secondAttributedString: [ctx2 rootObject]
-																							source: @"diff12"];
+																							source: @"branchA"];
 	
-    COAttributedStringDiff *diff13 = [[COAttributedStringDiff alloc] initWithFirstAttributedString: [ctx1 rootObject]
+    COAttributedStringDiff *diffB = [[COAttributedStringDiff alloc] initWithFirstAttributedString: [ctx1 rootObject]
 																			secondAttributedString: [ctx3 rootObject]
-																							source: @"diff13"];
+																							source: @"branchB"];
 	
-	[diff12 addOperationsFromDiff: diff13];
-	[diff12 applyToAttributedString: [ctx1 rootObject]];
+	COAttributedStringDiff *diffMerged = [diffA diffByMergingWithDiff: diffB];
 	
-	/*
-	 ctxExpected:
-	 
-	 "dabc"
-	   ^^
-	   bold
-	    ^^
-		italic
-	   	
-	 */
+	COObjectGraphContext *destCtx = [COObjectGraphContext new];
+	[destCtx setItemGraph: ctx1];
+
+	[diffMerged applyToAttributedString: [destCtx rootObject]];
+
+
+	COObjectGraphContext *expectedCtx = [COObjectGraphContext new];
+	[expectedCtx setItemGraph: ctx1];
+	[self clearAttributedString: [expectedCtx rootObject]];
+	[self appendHTMLString: result toAttributedString: [expectedCtx rootObject]];
 	
-	UKObjectsEqual(A(@"d", @"a",    @"b",          @"c"), [[ctx1 rootObject] valueForKeyPath: @"chunks.text"]);
-	UKObjectsEqual(A(S(),  S(@"b"), S(@"b", @"i"), S(@"i")), [[ctx1 rootObject] valueForKeyPath: @"chunks.attributes.htmlCode"]);
+	
+	COAttributedStringWrapper *actualWrapper = [[COAttributedStringWrapper alloc] initWithBacking: [destCtx rootObject]];
+	COAttributedStringWrapper *expectedWrapper = [[COAttributedStringWrapper alloc] initWithBacking: [expectedCtx rootObject]];
+	
+	UKObjectsEqual(expectedWrapper, actualWrapper);
+}
+
+- (void) testMergeOverlappingAttributeAdditions
+{
+	[self checkMergingBase: @"abc"
+			   withBranchA: @"<B>ab</B>c"
+			   withBranchB: @"da<I>bc</I>"
+					 gives: @"d<B>a<I>b</B>c</I>"];
 }
 
 - (void) testMergeOverlappingAttributeAdditions2
 {
-	/*
-	 ctx1:
-	 
-	 "Hello"
-	 
-	 */
-	
-	COObjectGraphContext *ctx1 = [self makeAttributedString];
-	[self appendString: @"Hello" htmlCode: nil toAttributedString: [ctx1 rootObject]];
-	
-	
-	/*
-	 ctx2:
-	 
-	 "Hello"
-	  ^^^^
-	 bold
-	 
-	 */
-	
-	COObjectGraphContext *ctx2 = [COObjectGraphContext new];
-	[ctx2 setItemGraph: ctx1];
-	COAttributedStringWrapper *ctx2Wrapper = [[COAttributedStringWrapper alloc] initWithBacking: [ctx2 rootObject]];
-	[self setFontTraits: NSFontBoldTrait inRange: NSMakeRange(0,4) inTextStorage: ctx2Wrapper];
-			
-	/*
-	 ctx3:
-	 
-	 "Hello"
-		^^^
-	 underline
-	 
-	 */
-	
-	COObjectGraphContext *ctx3 = [COObjectGraphContext new];
-	[ctx3 setItemGraph: ctx1];
-	
-	COAttributedStringWrapper *ctx3Wrapper = [[COAttributedStringWrapper alloc] initWithBacking: [ctx3 rootObject]];
-	[ctx3Wrapper setAttributes: @{ NSUnderlineStyleAttributeName : @(NSUnderlineStyleSingle) } range: NSMakeRange(2, 3)];
-
-	
-	
-	COAttributedStringDiff *diff12 = [[COAttributedStringDiff alloc] initWithFirstAttributedString: [ctx1 rootObject]
-																			secondAttributedString: [ctx2 rootObject]
-																							source: @"diff12"];
-	
-    COAttributedStringDiff *diff13 = [[COAttributedStringDiff alloc] initWithFirstAttributedString: [ctx1 rootObject]
-																			secondAttributedString: [ctx3 rootObject]
-																							source: @"diff13"];
-	
-	[diff13 addOperationsFromDiff: diff12];
-	[diff13 applyToAttributedString: [ctx1 rootObject]];
-	
-	/*
-	 ctxExpected:
-	 
-	 "Hello"
-	  ^^^^
-	 bold
-	    ^^^
-		underline
-	 
-	 */
-	
-	UKObjectsEqual(A(@"He",    @"ll",         @"o"), [[ctx1 rootObject] valueForKeyPath: @"chunks.text"]);
-	UKObjectsEqual(A(S(@"b"),  S(@"b", @"u"), S(@"u")), [[ctx1 rootObject] valueForKeyPath: @"chunks.attributes.htmlCode"]);
+	[self checkMergingBase: @"hello"
+			   withBranchA: @"<B>hell</B>o"
+			   withBranchB: @"he<I>llo</I>"
+					 gives: @"<B>he<I>ll</B>o</I>"];
 }
 
 - (void) testMergeEmptyDiffPlusAddCharacter
 {
-	COObjectGraphContext *ctx1 = [self makeAttributedString];
-	[self appendString: @"a" htmlCode: nil toAttributedString: [ctx1 rootObject]];
-
-	
-	COObjectGraphContext *ctx2 = [COObjectGraphContext new];
-	[ctx2 setItemGraph: ctx1];
-	/* same as ctx1 */
-
-	COObjectGraphContext *ctx3 = [COObjectGraphContext new];
-	[ctx3 setItemGraph: ctx1];
-	[self clearAttributedString: [ctx3 rootObject]];
-	[self appendString: @"ab" htmlCode: nil toAttributedString: [ctx3 rootObject]];
-	
-	
-	COAttributedStringDiff *diff12 = [[COAttributedStringDiff alloc] initWithFirstAttributedString: [ctx1 rootObject]
-																			secondAttributedString: [ctx2 rootObject]
-																							source: @"diff12"];
-	
-    COAttributedStringDiff *diff13 = [[COAttributedStringDiff alloc] initWithFirstAttributedString: [ctx1 rootObject]
-																			secondAttributedString: [ctx3 rootObject]
-																							source: @"diff13"];
-	
-	[diff13 addOperationsFromDiff: diff12];
-	[diff13 applyToAttributedString: [ctx1 rootObject]];
-	
-	UKObjectsEqual(@"ab", [(COAttributedString *)[ctx1 rootObject] string]);
+	[self checkMergingBase: @"a"
+			   withBranchA: @"a"
+			   withBranchB: @"ab"
+					 gives: @"ab"];
 }
 
 - (void) testMergeAdditions
 {
-	COObjectGraphContext *ctx1 = [self makeAttributedString];
-	[self appendString: @"a" htmlCode: nil toAttributedString: [ctx1 rootObject]];
-	
-	
-	COObjectGraphContext *ctx2 = [COObjectGraphContext new];
-	[ctx2 setItemGraph: ctx1];
-	[self appendString: @"bc" htmlCode: nil toAttributedString: [ctx2 rootObject]];
-	
-	COObjectGraphContext *ctx3 = [COObjectGraphContext new];
-	[ctx3 setItemGraph: ctx1];
-	[self appendString: @"b" htmlCode: nil toAttributedString: [ctx3 rootObject]];
-	
-	
-	COAttributedStringDiff *diff12 = [[COAttributedStringDiff alloc] initWithFirstAttributedString: [ctx1 rootObject]
-																			secondAttributedString: [ctx2 rootObject]
-																							source: @"diff12"];
-	
-    COAttributedStringDiff *diff13 = [[COAttributedStringDiff alloc] initWithFirstAttributedString: [ctx1 rootObject]
-																			secondAttributedString: [ctx3 rootObject]
-																							source: @"diff13"];
-	
-	[diff13 addOperationsFromDiff: diff12];
-	[diff13 applyToAttributedString: [ctx1 rootObject]];
-	
-	// FIXME: Not sure if this should be a conflict?
-	// It gives "abbc" which is wrong.
-#if 0
-	UKObjectsEqual(@"abc", [(COAttributedString *)[ctx1 rootObject] string]);
-#endif
+	[self checkMergingBase: @"a"
+			   withBranchA: @"ab"
+			   withBranchB: @"abc"
+					 gives: @"abbc"];
 }
 
 - (void) testMergeConflictingInserts
