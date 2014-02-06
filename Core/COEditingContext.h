940c79e4e0955415f486125da194833aa3b43070
Quentin Mathé <quentin.mathe@gmail.com>
2014-02-06 11:06:59 +0000

    Extended COEditingContext class description to discuss Metamodel, Change Tracking and Undo
    git-svn-id: http://svn.gna.org/svn/etoile/trunk/Etoile/Frameworks/CoreObject@10225 7b3f36aa-e6db-49c1-8b4c-66eac1997e64

--- Core/COEditingContext.h
+++ Core/COEditingContext.h
@@ -14,11 +14,11 @@
 
 /**
  * @group Core
- * @abstract An editing context exposes an in-memory snapshot of a CoreObject store,
- * allows the user to queue changes in memory and commit them atomically.
- * 
- * Its functionality is split across 5 main classes (there is a owner chain where each element
- * owns the one just below in the list):
+ * @abstract An editing context exposes an in-memory snapshot of a CoreObject 
+ * store, allows the user to queue changes in memory and commit them atomically. 
+ *
+ * Its functionality is split across 5 main classes (there is a owner chain 
+ * where each element owns the one just below in the list):
  *
  * <deflist>
  * <term>COEditingContext</term><desc>Entry point for opening and creating stores.
@@ -28,34 +28,95 @@
  * <term>COBranch</term><desc>A position on the history graph, with the revision
  * contents exposed as a COObjectGraphContext</desc>
  * <term>COObjectGraphContext</term><desc>manages COObject graph, tracks 
- * changes and handles reloading new states.</desc>
- * <term>COObject</term><desc>Mutable inner object.</desc>
+ * changes and handles reloading new states</desc>
+ * <term>COObject</term><desc>Mutable inner object</desc>
  * </deflist>
  *
  * CORevision also fits this set although it is not directly owned by one object.
  *
  * @section Common Use Cases
  *
- * Typically COEditingContext is used at application startup to create or
- * open a store (+contextWithURL:), when creating or accessing
- * persistent roots, and to commit changes in all persistent roots.
+ * Typically COEditingContext is used at application startup to create or open 
+ * a store (+contextWithURL:), when creating, deleting or accessing persistent 
+ * roots, and to track and commit changes in all persistent roots.
  *
+ * To create, delete or access persistent roots, see Creation and Deletion 
+ * sections in COPersistentRoot documentation.
+ *
+ * @section Metamodel
+ *
+ * For all the persistent root inner objects, a valid entity description must 
+ * exist in -[COEditingContext modelDescriptionRepository].
+ *
+ * The metamodel in the model description repository is passed downwards 
+ * from the editing context to the object graph contexts that manages the inner 
+ * objects (see -[COObjectGraphContext modelDescriptionRepository])
+ *
+ * To register a new entity description that describe a COObject subclass, 
+ * override +[NSObject newEntityDescription], and ETModelDescriptionRepository 
+ * will automatically register it on launch in 
+ * +[ETModelDescriptionRepository mainRepository] (the default repository used 
+ * by +contextWithURL:).
+ *
+ * To register a new entity description without writing a COObject subclass, 
+ * see ETModelDescriptionRepository documentation. To instantiate the correct 
+ * inner objects or entities, those can be passed to 
+ * -[COObject initWithEntityDescription:objectGraphContext:], as 
+ * -insertNewPersistentRootWithEntityName: does. For more details about inner 
+ * object initialization, see COObject.
+ *
+ * @section Change Tracking
+ *
+ * COEditingContext, COPersistentRoot, COBranch, and COObjectGraphContext 
+ * tracks the uncommitted or pending changes in their persistent properties, 
+ * and in their owned element among the 5 core classes (see the ownership list 
+ * at the beginning of the overview).
+ *
+ * For the editing context, -hasChanges computes the current change 
+ * tracking state based on its own changes and the changes in the persistent 
+ * roots, -[COPersistentRoot hasChanges] is based on its own changes and the 
+ * changes in the branches, and so on until reaching COObjectGraphContext. 
+ * Take note that the same recursive model applies to -discardChanges.
+ *
+ * For pending changes, each class has a distinct change tracking API, but 
+ * all declare the same basic methods: -hasChanges and -discardsChanges.
+ * 
  * @section Object Equality
  *
- * COEditingContext, COPersistentRoot, COBranch, and COObjectGraphContext
- * do not override -hash or -isEqual:, so instances of these classes are only
+ * COEditingContext, COPersistentRoot, COBranch, and COObjectGraphContext do 
+ * not override -hash or -isEqual:, so instances of these classes are only
  * considered equal to the same instances.
  *
- * These classes form an in-memory view on a database, and the notion
- * of two of these views being equal isn't useful or interesting.
+ * These classes form an in-memory view on a database, and the notion of two of 
+ * these views being equal isn't useful or interesting.
  *
- * @section Commits
+ * @section Commits and Undo
  *
  * In the current implementation, all changes made in a COEditingContext are
  * committed atomically in one SQLite transaction. However, when you consider 
- * CoreObject as a version control system, atomicity only exists
- * per-persistent root, since persistent roots are the units of versioning and
- * each can be manipulated independently (rolled back, etc.)
+ * CoreObject as a version control system, atomicity only exists per-persistent 
+ * root, since persistent roots are the units of versioning and each can be 
+ * manipulated independently (rolled back, etc.).
+ *
+ * For committing changes, see -commitWithIdentifier:metadata:undoTrack:error: 
+ * and other similar commit methods. Take note that COCommitDescriptor provides 
+ * support to localize the commit metadata.
+ *
+ * For recording these commits as undoable actions, see COUndoTrack. CoreObject 
+ * supports two undo/redo models that both conform to the COTrack protocol: 
+ * 
+ * <list>
+ * <item>a branch undo/redo model, that moves the current revision pointer on 
+ * the branch path (this model cannot undo/redo commits involving changes that 
+ * don't create a new revision, or span several persistent roots or 
+ * branches)</item>
+ * <item>a rich undo/redo model based on recording commits on an undo track 
+ * (and even aggregating commits accross multiple undo tracks)</item>
+ * </list>
+ *
+ * In most cases, to support undo/redo in an application, use a COUndoTrack. 
+ * Branch undo/redo can be used to inspect and navigate a single persistent 
+ * root history (e.g. in a timeline UI presenting a document history). 
  */
 @interface COEditingContext : NSObject <COPersistentObjectContext>
 {
@@ -151,6 +212,10 @@
  * Returns the persistent root bound the the given UUID in the store or nil.
  *
  * The editing context retains the returned persistent root.
+ *
+ * This method can return any persistent roots among -persistentRoots and 
+ * -deletedPersistentRoots (including those pending deletion and undeletion), 
+ * but the loading is restricted to the requested persistent root.
  */
 - (COPersistentRoot *)persistentRootForUUID: (ETUUID *)aUUID;
 /**
@@ -161,6 +226,9 @@
  *
  * The object graph context of the root object must be transient, otherwise 
  * a NSInvalidArgumentException is raised.
+ *
+ * The object graph context of the root object must also use the same model 
+ * description repository than the receiver.
  *
  * For a nil root object, raises a NSInvalidArgumentException.
  */
@@ -173,6 +241,7 @@
 
 
 /** @taskunit Pending Changes */
+
 
 /**
  * The new persistent roots to be saved in the store on the next commit.
