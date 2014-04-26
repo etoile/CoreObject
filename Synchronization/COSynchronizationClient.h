6bffcb79556a3e6ece63642960da4084b8db937e
Eric Wasylishen <ewasylishen@gmail.com>
2014-04-25 22:31:38 -0600

    Add comment explaining the COSynchronizationClient class

--- Synchronization/COSynchronizationClient.h
+++ Synchronization/COSynchronizationClient.h
@@ -9,6 +9,15 @@
 #import <CoreObject/CoreObject.h>
 #import <EtoileFoundation/EtoileFoundation.h>
 
+/**
+ * This class and COSynchronizationServer were a rough sketch of how we could
+ * replicate persistent roots between stores, and fetch updates after an
+ * initial replication. It doesn't handle merging changes so it's not very
+ * useful.
+ *
+ * It remains here because it would be a good thing to have finished at some point,
+ * and the tests exercised some store functionality that wasn't well tested elsewhere.
+ */
 @interface COSynchronizationClient : NSObject
 
 - (NSDictionary *) updateRequestForPersistentRoot: (ETUUID *)aRoot
