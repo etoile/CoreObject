f8d2d0cc9e3c987a03b260e700bf421f45e52658
Quentin Mathé <quentin.mathe@gmail.com>
2014-06-09 14:45:47 +0200

    Fixed package.sh to copy etoile.make in UnitKit, EtoileFoundation and CoreObject directories

--- Scraps/package.sh
+++ Scraps/package.sh
@@ -6,9 +6,14 @@ ARCHIVENAME="CoreObject-0.5.tgz"
 rm -fr $CHECKOUTDIR
 mkdir $CHECKOUTDIR
 cd $CHECKOUTDIR
+git clone https://github.com/etoile/Etoile.git
 git clone https://github.com/etoile/CoreObject.git
 git clone https://github.com/etoile/EtoileFoundation.git
 git clone https://github.com/etoile/UnitKit.git
+
+cp Etoile/etoile.make CoreObject 
+cp Etoile/etoile.make EtoileFoundation
+cp Etoile/etoile.make UnitKit
 
 rm -fr CoreObject/.git
 rm -fr EtoileFoundation/.git
@@ -16,6 +21,6 @@ rm -fr UnitKit/.git
 
 tar czvvf $ARCHIVENAME CoreObject EtoileFoundation UnitKit
 
-rm -fr CoreObject EtoileFoundation UnitKit
+rm -fr Etoile CoreObject EtoileFoundation UnitKit
 cd ..
 
