d14e5aeb606944ef000b264b4ae7499a3b76a504
Quentin Mathé <qmathe@gmail.com>
2014-06-28 13:10:35 +0200

    Added COClassToString transformer

---
+++ Extras/ValueTransformers/COClassToString.m
@@ -0,0 +1,47 @@
+/*
+	Copyright (C) 2014 Quentin Mathe
+
+	Date:  May 2014
+	License:  MIT  (see COPYING)
+ */
+
+#import "COClassToString.h"
+#include <objc/runtime.h>
+
+@implementation COClassToString
+
++ (Class)transformedValueClass
+{
+	return [NSString class];
+}
+
++ (BOOL)allowsReverseTransformation
+{
+	return YES;
+}
+
+- (id)transformedValue: (id)value
+{
+    if (value == Nil)
+        return nil;
+
+    BOOL isClass = ([value class] == value);
+    NSParameterAssert(isClass && !class_isMetaClass(value));
+
+	return NSStringFromClass(value);
+}
+
+- (id)reverseTransformedValue: (id)value
+{
+    if (value == nil)
+        return Nil;
+
+	NSParameterAssert([value isKindOfClass: [NSString class]]);
+
+	Class class = NSClassFromString(value);
+    ETAssert(class != Nil);
+    ETAssert([NSStringFromClass(class) isEqual: value]);
+    return class;
+}
+
+@end
