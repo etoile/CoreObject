a177c21941c1300a47f7b6409d15358d682c607e
Quentin Mathé <quentin.mathe@gmail.com>
2015-06-22 17:39:40 +0200

    More progresses on history compaction
    Changed live/dead revision set to be tracked by persistent root
    
    Fixed logic in -scanRevisionInLiveCommand: (was more complex than necessary)
    
    Added -compactHistory: to COSQLiteStore copied/pasted from -finalizeDeletions:, but with some adjustments to be compatible with COHistoryCompaction

--- Store/COHistoryCompaction.h
+++ Store/COHistoryCompaction.h
@@ -7,6 +7,7 @@
 
 #import <Foundation/Foundation.h>
 #import <EtoileFoundation/EtoileFoundation.h>
+#import <CoreObject/COSQLiteStore.h>
 
 @class COUndoTrack, COCommand;
 
@@ -17,8 +18,8 @@
 	COCommand *_oldestCommandToKeep;
 	NSMutableSet *_deadPersistentRootUUIDs;
 	NSMutableSet *_livePersistentRootUUIDs;
-	NSMutableSet *_deadRevisionUUIDs;
-	NSMutableSet *_liveRevisionUUIDs;
+	NSMutableDictionary *_deadRevisionUUIDs;
+	NSMutableDictionary *_liveRevisionUUIDs;
 }
 
 - (instancetype)initWithUndoTrack: (COUndoTrack *)aTrack upToCommand: (COCommand *)aCommand;
@@ -31,4 +32,48 @@
  */
 - (void)compute;
 
+/**
+ * @taskunit Computed Results
+ */
+
+/**
+ * The persistent roots to be deleted when compacting the history.
+ */
+@property (nonatomic, readonly) NSSet *deadPersistentRootUUIDs;
+/**
+ * The persistent roots to be kept when compacting the history, but whose 
+ * branches and revisions can be deleted.
+ *
+ * Peristent roots not included in this set won't have their revision examined, 
+ * when the history is compacted.
+ */
+@property (nonatomic, readonly) NSSet *livePersistentRootUUIDs;
+/**
+ * The deletable revision sets when compacting the history, organized by 
+ * persistent root UUID.
+ *
+ * These revisions won't be deleted, when they are inside a live revision range. 
+ * See -liveRevisionUUIDs.
+ */
+@property (nonatomic, readonly) NSDictionary *deadRevisionUUIDs;
+/**
+ * The revisions sets to be kept when compacting the history, organized by 
+ * persistent root UUID.
+ *
+ * These revisions can include dead revisions in their range, see 
+ * -deadRevisionUUIDs.
+ */
+@property (nonatomic, readonly) NSDictionary *liveRevisionUUIDs;
+
+- (NSSet *)deadRevisionUUIDsForPersistentRootUUIDs: (NSArray *)persistentRootUUIDs;
+- (NSSet *)liveRevisionUUIDsForPersistentRootUUIDs: (NSArray *)persistentRootUUIDs;
+
+@end
+
+
+@interface COSQLiteStore (COHistoryCompaction)
+/**
+ * Compacts the history with the given strategy.
+ */
+- (BOOL)compactHistory: (COHistoryCompaction *)aCompactionStrategy;
 @end
--- Store/COHistoryCompaction.m
+++ Store/COHistoryCompaction.m
@@ -10,21 +10,28 @@
 #import "COCommandDeletePersistentRoot.h"
 #import "COCommandSetCurrentVersionForBranch.h"
 #import "COCommandUndeletePersistentRoot.h"
+#import "COSQLiteStorePersistentRootBackingStore.h"
 #import "COUndoTrack.h"
+#import "FMDatabase.h"
+#import "FMDatabaseAdditions.h"
+
+#define PERSISTENT_ROOT_CAPACITY_HINT 25000
 
 @implementation COHistoryCompaction
 
-@synthesize undoTrack = _undoTrack;
+@synthesize undoTrack = _undoTrack, deadPersistentRootUUIDs = _deadPersistentRootUUIDs,
+	livePersistentRootUUIDs = _livePersistentRootUUIDs,
+	deadRevisionUUIDs = _deadRevisionUUIDs, liveRevisionUUIDs = _liveRevisionUUIDs;
 
 - (instancetype)initWithUndoTrack: (COUndoTrack *)aTrack upToCommand: (COCommand *)aCommand
 {
 	SUPERINIT;
 	_undoTrack = aTrack;
 	_oldestCommandToKeep = aCommand;
-	_deadPersistentRootUUIDs = [NSMutableSet setWithCapacity: 50000];
-	_livePersistentRootUUIDs = [NSMutableSet setWithCapacity: 50000];
-	_deadRevisionUUIDs = [NSMutableSet setWithCapacity: 500000];
-	_liveRevisionUUIDs = [NSMutableSet setWithCapacity: 500000];
+	_deadPersistentRootUUIDs = [NSMutableSet setWithCapacity: PERSISTENT_ROOT_CAPACITY_HINT];
+	_livePersistentRootUUIDs = [NSMutableSet setWithCapacity: PERSISTENT_ROOT_CAPACITY_HINT];
+	_deadRevisionUUIDs = [NSMutableDictionary dictionaryWithCapacity: PERSISTENT_ROOT_CAPACITY_HINT];
+	_liveRevisionUUIDs = [NSMutableDictionary dictionaryWithCapacity: PERSISTENT_ROOT_CAPACITY_HINT];
 	return self;
 }
 
@@ -56,7 +63,19 @@
 	}
 }
 
-	
+/**
+ * A this point, we know the exact dead and live persistent root sets, so we
+ * we don't have to collect revisions for dead persistent roots.
+ */
+- (void)allocateRevisionSets
+{
+	for (ETUUID *persistentRootUUID in _livePersistentRootUUIDs)
+	{
+		_deadRevisionUUIDs[persistentRootUUID] = [NSMutableSet set];
+		_liveRevisionUUIDs[persistentRootUUID] = [NSMutableSet set];
+	}
+}
+
 /** 
  * Scanning to decide which revisions are alive, based on whether their
  * branch or persistent root are alive as computed by -scanPersistentRoots.
@@ -64,6 +83,8 @@
 - (void)scanRevisions
 {
 	BOOL isScanningLiveCommands = NO;
+	
+	[self allocateRevisionSets];
 
 	for (COCommand *command in [_undoTrack.allCommands reverseObjectEnumerator])
 	{
@@ -124,13 +145,15 @@
 
 - (void)scanRevisionInDeadCommand: (id)command
 {
+	ETUUID *persistentRootUUID = [command persistentRootUUID];
+
 	if ([command isKindOfClass: [COCommandSetCurrentVersionForBranch class]])
 	{
-		[_deadRevisionUUIDs addObject: [command revisionUUID]];
+		[_deadRevisionUUIDs[persistentRootUUID] addObject: [command revisionUUID]];
 	}
 	else if ([command isKindOfClass: [COCommandUndeletePersistentRoot class]])
 	{
-		[_deadRevisionUUIDs removeObject: [command initialRevisionID]];
+		[_deadRevisionUUIDs[persistentRootUUID] removeObject: [command initialRevisionID]];
 	}
 	
 	ETAssert(_liveRevisionUUIDs.isEmpty);
@@ -138,34 +161,205 @@
 
 - (void)scanRevisionInLiveCommand: (id)command
 {
+	ETUUID *persistentRootUUID = [command persistentRootUUID];
+
 	if ([command isKindOfClass: [COCommandSetCurrentVersionForBranch class]])
 	{
-		BOOL isDeadPersistentRoot =
-			[_deadPersistentRootUUIDs containsObject: [command persistentRootUUID]];
-		NSMutableSet *addedRevisionUUIDs;
-		NSMutableSet *removedRevisionUUIDs;
+		ETAssert(![_deadPersistentRootUUIDs containsObject: [command persistentRootUUID]]);
 
 		// TODO: We'll need something more precise when we check branch aliveness
-		addedRevisionUUIDs = isDeadPersistentRoot ? _deadRevisionUUIDs : _liveRevisionUUIDs;
-		removedRevisionUUIDs = isDeadPersistentRoot ? _liveRevisionUUIDs : _deadRevisionUUIDs;
 
-		[addedRevisionUUIDs addObject: [command oldRevisionUUID]];
-		[removedRevisionUUIDs removeObject: [command oldRevisionUUID]];
-		
-		[addedRevisionUUIDs addObject: [command revisionUUID]];
-		[removedRevisionUUIDs removeObject: [command revisionUUID]];
-		
-		[addedRevisionUUIDs addObject: [command oldHeadRevisionUUID]];
-		[removedRevisionUUIDs removeObject: [command oldHeadRevisionUUID]];
-		
-		[addedRevisionUUIDs addObject: [command headRevisionUUID]];
-		[removedRevisionUUIDs removeObject: [command headRevisionUUID]];
+		[_deadRevisionUUIDs[persistentRootUUID] removeObject: [command oldRevisionUUID]];
+		[_liveRevisionUUIDs[persistentRootUUID] addObject: [command oldRevisionUUID]];
+
+		[_deadRevisionUUIDs[persistentRootUUID] removeObject: [command revisionUUID]];
+		[_liveRevisionUUIDs[persistentRootUUID] addObject: [command revisionUUID]];
+
+		[_deadRevisionUUIDs[persistentRootUUID] removeObject: [command oldHeadRevisionUUID]];
+		[_liveRevisionUUIDs[persistentRootUUID] addObject: [command oldHeadRevisionUUID]];
+
+		[_deadRevisionUUIDs[persistentRootUUID] removeObject: [command headRevisionUUID]];
+		[_liveRevisionUUIDs[persistentRootUUID] addObject: [command headRevisionUUID]];
 	}
 	else if ([command isKindOfClass: [COCommandUndeletePersistentRoot class]])
 	{
-		[_deadRevisionUUIDs removeObject: [command initialRevisionID]];
-		[_liveRevisionUUIDs addObject: [command initialRevisionID]];
+		[_deadRevisionUUIDs[persistentRootUUID] removeObject: [command initialRevisionID]];
+		[_liveRevisionUUIDs[persistentRootUUID] addObject: [command initialRevisionID]];
 	}
+}
+
+- (NSSet *)deadRevisionUUIDsForPersistentRootUUIDs: (NSArray *)persistentRootUUIDs
+{
+	NSMutableSet *revisionUUIDs = [NSMutableSet new];
+	
+	for (NSSet *revisionSet in [_deadRevisionUUIDs objectsForKeys: persistentRootUUIDs
+	                                               notFoundMarker: nil])
+	{
+		[revisionUUIDs unionSet: revisionSet];
+	}
+	return revisionUUIDs;
+}
+
+- (NSSet *)liveRevisionUUIDsForPersistentRootUUIDs: (NSArray *)persistentRootUUIDs
+{
+	NSMutableSet *revisionUUIDs = [NSMutableSet new];
+	
+	for (NSSet *revisionSet in [_liveRevisionUUIDs objectsForKeys: persistentRootUUIDs
+	                                               notFoundMarker: nil])
+	{
+		[revisionUUIDs unionSet: revisionSet];
+	}
+	return revisionUUIDs;
+}
+
+@end
+
+
+@interface COSQLiteStore ()
+- (void)deleteBackingStoreWithUUID: (ETUUID *)aUUID;
+- (COSQLiteStorePersistentRootBackingStore *) backingStoreForUUID: (ETUUID *)aUUID error: (NSError **)error;
+- (BOOL)finalizeGarbageAttachments;
+@end
+
+@implementation COSQLiteStore (COHistoryCompaction)
+
+- (BOOL)compactHistory: (COHistoryCompaction *)aCompactionStrategy
+{
+    NILARG_EXCEPTION_TEST(aCompactionStrategy);
+    ETAssert(dispatch_get_current_queue() != queue_);
+    
+    dispatch_sync(queue_, ^()
+	{
+        [db_ beginTransaction];
+        
+        // Delete rows from branches and persistentroots tables
+		
+		for (ETUUID *persistentRoot in aCompactionStrategy.deadPersistentRootUUIDs)
+		{
+			NSData *persistentRootData = [persistentRoot dataValue];
+			
+			[db_ executeUpdate: @"DELETE FROM branches WHERE deleted = 1 AND proot = ?", persistentRootData];
+			
+			if ([db_ boolForQuery: @"SELECT deleted FROM persistentroots WHERE uuid = ?", persistentRootData])
+			{
+				[db_ executeUpdate: @"DELETE FROM branches WHERE proot = ?", persistentRootData];
+				[db_ executeUpdate: @"DELETE FROM persistentroots WHERE uuid = ?", persistentRootData];
+			}
+		}
+
+		// Delete unused backing stores
+		
+		NSArray *unusedBackingstoreUUIDDatas =
+			[db_ arrayForQuery: @"SELECT backingstore "
+			 "FROM persistentroot_backingstores "
+			 "LEFT OUTER JOIN (SELECT uuid, 1 AS present FROM persistentroots) USING(uuid) "
+			 "GROUP BY backingstore "
+			 "HAVING 0 = COALESCE(SUM(present), 0)"];
+		for (NSData *backingstoreUUIDData in unusedBackingstoreUUIDDatas)
+		{
+			[self deleteBackingStoreWithUUID: [ETUUID UUIDWithData: backingstoreUUIDData]];
+		}
+		
+		// Gather backing stores that need revision GC
+		
+		NSMutableSet *backingStoresForRevisionGC = [NSMutableSet new];
+		NSMutableDictionary *persistentRootsByBackingStore = [NSMutableDictionary new];
+
+		for (ETUUID *persistentRoot in aCompactionStrategy.livePersistentRootUUIDs)
+		{
+			NSData *persistentRootData = [persistentRoot dataValue];
+			NSData *backingStoreData = [db_ dataForQuery: @"SELECT backingstore FROM persistentroot_backingstores WHERE uuid = ?", persistentRootData];
+						
+			// Don't attempt revision GC on backing stores we just deleted
+			if (![unusedBackingstoreUUIDDatas containsObject: backingStoreData])
+			{
+				ETUUID *backingStore = [ETUUID UUIDWithData: backingStoreData];
+
+				[backingStoresForRevisionGC addObject: backingStore];
+				
+				if (persistentRootsByBackingStore[backingStore] == nil)
+				{
+					persistentRootsByBackingStore[backingStore] = [NSMutableArray new];
+				}
+				[persistentRootsByBackingStore[backingStore] addObject: persistentRoot];
+			}
+		}
+		
+		// Do the revision GC
+		
+		for (ETUUID *backingUUID in backingStoresForRevisionGC)
+		{
+			NSData *backingUUIDData = [backingUUID dataValue];
+		
+			COSQLiteStorePersistentRootBackingStore *backing = [self backingStoreForUUID: backingUUID error: NULL];
+			
+			// Delete just the unreachable revisions
+			
+			NSArray *persistentRootUUIDs = persistentRootsByBackingStore[backingUUID];
+			NSSet *liveRevisionUUIDs = [aCompactionStrategy liveRevisionUUIDsForPersistentRootUUIDs: persistentRootUUIDs];
+			NSIndexSet *keptRevisions = nil;
+			NSIndexSet *revisions = [backing revidsUsedRange];
+			
+			if (liveRevisionUUIDs.isEmpty)
+			{
+				FMResultSet *rs = [db_ executeQuery: @"SELECT "
+								   "branches.current_revid "
+								   "FROM persistentroots "
+								   "INNER JOIN branches ON persistentroots.uuid = branches.proot "
+								   "INNER JOIN persistentroot_backingstores ON persistentroots.uuid = persistentroot_backingstores.uuid "
+								   "WHERE persistentroot_backingstores.backingstore = ?", backingUUIDData];
+				
+				keptRevisions = [NSMutableIndexSet new];
+
+				while ([rs next])
+				{
+					ETUUID *head = [ETUUID UUIDWithData: [rs dataForColumnIndex: 0]];
+					
+					NSIndexSet *revs = [backing revidsFromRevid: 0
+														toRevid: [backing revidForUUID: head]];
+					[(NSMutableIndexSet *)keptRevisions addIndexes: revs];
+				}
+				[rs close];
+			}
+			else
+			{
+				NSIndexSet *liveRevisions = [backing revidsForUUIDs: liveRevisionUUIDs.allObjects];
+				
+				/* Join the live revisions into a single range to determine the kept revisions */
+				ETAssert(liveRevisions.lastIndex <= revisions.lastIndex);
+				NSUInteger length = revisions.lastIndex - liveRevisions.firstIndex + 1;
+				NSRange liveRange = NSMakeRange(liveRevisions.firstIndex, length);
+				
+				keptRevisions = [NSIndexSet indexSetWithIndexesInRange: liveRange];
+			}
+			
+			// Now for each index set in deletedRevisionsForBackingStore, subtract the index set
+			// in keptRevisionsForBackingStore
+			
+			NSMutableIndexSet *deletedRevisions = [NSMutableIndexSet indexSet];
+			[deletedRevisions addIndexes: revisions];
+			[deletedRevisions removeIndexes: keptRevisions];
+			
+			//    for (NSUInteger i = [deletedRevisions firstIndex]; i != NSNotFound; i = [deletedRevisions indexGreaterThanIndex: i])
+			//    {
+			//
+			//        [db_ executeUpdate: @"DELETE FROM attachment_refs WHERE root_id = ? AND revid = ?",
+			//         [backingUUID dataValue],
+			//         [NSNumber numberWithLongLong: i]];
+			//
+			//        // FIXME: FTS, proot_refs
+			//    }
+			
+			// Delete the actual revisions
+			assert([backing deleteRevids: deletedRevisions]);
+		}
+		
+		assert([db_ commit]);
+		
+        [self finalizeGarbageAttachments];
+    });
+    
+    return YES;
 }
 
 @end
