9e4024e0f25fa374ef3217dceaa05222275210ac
Quentin Mathé <quentin.mathe@gmail.com>
2015-07-10 11:47:37 +0200

    Added COURLToString serialization transformer

---
+++ Extras/ValueTransformers/COURLToString.m
@@ -0,0 +1,42 @@
+/*
+	Copyright (C) 2015 Quentin Mathe
+
+	Date:  July 2015
+	License:  MIT  (see COPYING)
+ */
+
+#import "COURLToString.h"
+
+@implementation COURLToString
+
++ (Class)transformedValueClass
+{
+	return [NSString class];
+}
+
++ (BOOL)allowsReverseTransformation
+{
+	return YES;
+}
+
+- (id)transformedValue: (id)value
+{
+	if (value == nil)
+		return nil;
+
+	NSParameterAssert([value isKindOfClass: [NSURL class]]);
+
+	return [(NSURL *)value absoluteString];
+}
+
+- (id)reverseTransformedValue: (id)value
+{
+	if (value == nil)
+		return nil;
+
+	NSParameterAssert([value isKindOfClass: [NSString class]]);
+
+	return [NSURL URLWithString: (NSString *)value];
+}
+
+@end
