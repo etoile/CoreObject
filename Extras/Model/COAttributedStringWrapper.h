88831f17e83d6b4ea8cac9f1a70915dde4ec4874
Quentin Mathé <quentin.mathe@gmail.com>
2015-10-09 11:10:59 +0200

    This should fix the build on OS X versions older than 10.11

--- Extras/Model/COAttributedStringWrapper.h
+++ Extras/Model/COAttributedStringWrapper.h
@@ -6,6 +6,10 @@
  */
 
 #import <Foundation/Foundation.h>
+// For building on versions earlier than 10.11
+#ifndef MAC_OS_X_VERSION_10_11
+#	define MAC_OS_X_VERSION_10_11 1011
+#endif
 #if (TARGET_OS_IPHONE)
 #	import <CoreObject/COCocoaTouchCompatibility.h>
 #else
