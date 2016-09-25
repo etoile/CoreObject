10c3335c3decbb8e43a133794c38a253022f68b8
Quentin Mathé <quentin.mathe@gmail.com>
2016-09-25 13:14:43 +0200

    Extracted COEditingContext debugging support into a distinct implementation file

--- Core/COEditingContext.m
+++ Core/COEditingContext.m
@@ -1051,58 +1051,6 @@ restrictedToPersistentRoots: (NSArray *)persistentRoots
 @end
 
 
-@implementation COEditingContext (Debugging)
-
-- (NSArray *)loadedObjects
-{
-	return [self arrayByCollectingObjectsFromPersistentRootsUsingSelector: @selector(loadedObjects)];
-}
-
-- (NSArray *)loadedRootObjects
-{
-	NSMutableArray *collectedObjects = [NSMutableArray new];
-
-	for (COPersistentRoot *persistentRoot in [_loadedPersistentRoots objectEnumerator])
-	{
-		for (COObjectGraphContext *objectGraphContext in persistentRoot.allObjectGraphContexts)
-		{
-			[collectedObjects addObject: objectGraphContext.rootObject];
-		}
-	}
-	return collectedObjects;
-}
-
-- (NSArray *)arrayByCollectingObjectsFromPersistentRootsUsingSelector: (SEL)aSelector
-{
-	NSMutableArray *collectedObjects = [NSMutableArray new];
-
-	for (COPersistentRoot *persistentRoot in [_loadedPersistentRoots objectEnumerator])
-	{
-		for (COObjectGraphContext *objectGraphContext in persistentRoot.allObjectGraphContexts)
-		{
-			[collectedObjects addObjectsFromArray: [objectGraphContext performSelector: aSelector]];
-		}
-	}
-	return collectedObjects;
-}
-
-- (NSArray *)insertedObjects
-{
-	return [self arrayByCollectingObjectsFromPersistentRootsUsingSelector: @selector(insertedObjects)];
-}
-
-- (NSArray *)updatedObjects
-{
-	return [self arrayByCollectingObjectsFromPersistentRootsUsingSelector: @selector(updatedObjects)];
-}
-
-- (NSArray *)changedObjects
-{
-	return [self arrayByCollectingObjectsFromPersistentRootsUsingSelector: @selector(changedObjects)];
-}
-
-@end
-
 NSString *const COEditingContextDidChangeNotification =
 	@"COEditingContextDidChangeNotification";
 NSString *const kCOCommandKey = @"kCOCommandKey";
