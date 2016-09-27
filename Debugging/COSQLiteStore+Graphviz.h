bb33299db2cf564431bb4a5518ed10d48e1b3f71
Eric Wasylishen <ewasylishen@gmail.com>
2016-09-27 14:59:52 -0600

    Tweak to Debugging formatting

--- Debugging/COSQLiteStore+Graphviz.h
+++ Debugging/COSQLiteStore+Graphviz.h
@@ -14,7 +14,6 @@ void COViewDOTGraphFile(NSString *path);
 @interface COSQLiteStore (Debugging)
 
 - (NSString *)dotGraphForPersistentRootUUID: (ETUUID *)aUUID;
-
 - (void)showGraphForPersistentRootUUID: (ETUUID *)aUUID;
 
 @end
