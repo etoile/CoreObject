a9ab343ac58e7d093d353e747cc44c0801615d96
Quentin Mathé <quentin.mathe@gmail.com>
2016-10-04 02:21:46 +0200

    Cleaned and reorganized

--- Core/COBranch+Private.h
+++ Core/COBranch+Private.h
@@ -11,70 +11,56 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface COBranch ()
 
-/**
- * This method is only exposed to be used internally by CoreObject.
- */
+
+/** @taskunit Initialization */
+
+
 - (instancetype)initWithUUID: (ETUUID *)aUUID
               persistentRoot: (COPersistentRoot *)aPersistentRoot
             parentBranchUUID: (nullable ETUUID *)aParentBranchUUID
   parentRevisionForNewBranch: (nullable ETUUID *)parentRevisionForNewBranch NS_DESIGNATED_INITIALIZER;
+
 @property (nonatomic, readonly) COSQLiteStore *store;
-/**
- * This method is only exposed to be used internally by CoreObject.
- */
+
 - (void)updateWithBranchInfo: (COBranchInfo *)branchInfo
                    compacted: (BOOL)wasCompacted;
-/**
- * This method is only exposed to be used internally by CoreObject.
- */
+
 @property (nonatomic, readonly, strong) COBranchInfo *branchInfo;
-/**
- * This method is only exposed to be used internally by CoreObject.
- */
+
+
+/** @taskunit Status */
+
+
 @property (nonatomic, readonly, getter=isDeletedInStore) BOOL deletedInStore;
-/**
- * This method is only exposed to be used internally by CoreObject.
- *
- * Forces the branch to reload an older or more recent state.
- *
- * The public API -setCurrentRevision: uses this method.
- */
-- (void)reloadAtRevision: (CORevision *)revision;
-/**
- * This method is only exposed to be used internally by CoreObject.
- */
-- (void)didMakeInitialCommitWithRevisionUUID: (ETUUID *)aRevisionUUID
-                                 transaction: (COStoreTransaction *)txn;
-/**
- * This method is only exposed to be used internally by CoreObject.
- */
-- (void)saveCommitWithMetadata: (nullable NSDictionary<NSString *, id> *)metadata
-                   transaction: (COStoreTransaction *)txn;
-/**
- * This method is only exposed to be used internally by CoreObject.
- */
-- (void)saveDeletionWithTransaction: (COStoreTransaction *)txn;
-/**
- * This method is only exposed to be used internally by CoreObject.
- */
 @property (nonatomic, readonly, getter=isBranchUncommitted) BOOL branchUncommitted;
 @property (nonatomic, readonly, getter=isBranchPersistentRootUncommitted) BOOL branchPersistentRootUncommitted;
-/**
- * This method is only exposed to be used internally by CoreObject.
- */
-- (void)updateRevisions: (BOOL)reload;
-/**
- * This method is only exposed to be used internally by CoreObject.
- *
- * A head revision change is saved on the next branch commit.
- */
-@property (nonatomic, readwrite, nullable) CORevision *headRevision;
+
+
+/** @taskunit Object Graph */
+
 
 @property (nonatomic, readonly, strong) COObjectGraphContext *objectGraphContextWithoutUnfaulting;
 @property (nonatomic, readonly) BOOL objectGraphContextHasChanges;
+
+
+/** @taskunit Committing Changes */
+
+
+- (void)saveCommitWithMetadata: (nullable NSDictionary<NSString *, id> *)metadata
+                   transaction: (COStoreTransaction *)txn;
+- (void)saveDeletionWithTransaction: (COStoreTransaction *)txn;
+- (void)didMakeInitialCommitWithRevisionUUID: (ETUUID *)aRevisionUUID
+                                 transaction: (COStoreTransaction *)txn;
+
+
+/** @taskunit Revisions */
+
+
 /**
- * This method is only exposed to be used internally by CoreObject.
- *
+ * A head revision change is saved on the next branch commit.
+ */
+@property (nonatomic, readwrite, nullable) CORevision *headRevision;
+/**
  * Same as -setCurrentRevision:, but doesn't check the supportsRevert
  * property. This is used by COSynchronizerServer/COSynchronizerClient,
  * which need to violate the supportsRevert flag.
@@ -82,6 +68,13 @@ NS_ASSUME_NONNULL_BEGIN
  * The public API -setCurrentRevision: uses this method.
  */
 - (void)setCurrentRevisionSkipSupportsRevertCheck: (CORevision *)currentRevision;
+/**
+ * Forces the branch to reload an older or more recent state.
+ *
+ * The public API -setCurrentRevision: uses this method.
+ */
+- (void)reloadAtRevision: (CORevision *)revision;
+- (void)updateRevisions: (BOOL)reload;
 
 @end
 
