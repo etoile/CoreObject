f11aa84a1778b09562a4540d2cddabad6d8552fb
Quentin Mathé <quentin.mathe@gmail.com>
2024-03-05 10:58:16 +0100

    Write more benchmarks

--- TestRevisionRewritingPerformance.m
+++ TestRevisionRewritingPerformance.m
@@ -29,12 +29,15 @@
 
 - (NSArray *)commitPersistentRootsWithEntityName: (NSString *)entityName
                                            count: (int)nbOfPersistentRoots
+                                    studentCount: (int)studentCount
 {
     NSMutableArray *proots = [NSMutableArray new];
 
     for (int i = 0; i < nbOfPersistentRoots; i++)
     {
-        [proots addObject: [ctx insertNewPersistentRootWithEntityName: entityName]];
+        COPersistentRoot *proot = [ctx insertNewPersistentRootWithEntityName: entityName];
+        [self addNewStudents: studentCount toPerson: proot.rootObject];
+        [proots addObject: proot];
         revCounter++;
     }
     [ctx commit];
@@ -42,7 +45,7 @@
     return proots;
 }
 
-- (void)updatePerson: (Person *)person includesNewStudents: (BOOL)includesNewStudents
+- (void)updatePerson: (Person *)person
 {
     uint32_t draw = arc4random_uniform(3);
     
@@ -73,41 +76,48 @@
         default:
             break;
     }
+}
 
-    if (!includesNewStudents)
+- (void)addNewStudents: (int)count toPerson: (Person *)person
+{
+    if (count == 0)
+    {
         return;
-
+    }
     NSMutableSet *students = [person.students mutableCopy];
 
-    for (int i = 0; i < 1; i++)
+    for (int i = 0; i < count; i++)
     {
         Person *student = [[Person alloc] initWithObjectGraphContext: person.objectGraphContext];
 
-        [self updatePerson: student includesNewStudents: NO];
+        [self updatePerson: person];
         [students addObject: student];
     }
     person.students = students;
 }
 
-- (void)makeCommitToPersistentRoots: (NSArray *)proots
+- (void)makeCommitToPersistentRoots: (NSArray *)proots studentCount: (NSInteger)count
 {
     for (COPersistentRoot *proot in proots)
     {
         Person *person = (Person *)[proot rootObject];
 
-        [self updatePerson: person includesNewStudents: YES];
+        [self updatePerson: person];
+        [self addNewStudents: count toPerson: person];
+
         revCounter++;
     }
     [ctx commit];
 }
 
-- (void)testRewriteStoreRevisions
+- (void)testSingleItem
 {
     ctx.recordingUndo = NO;
 
     NSDate *startDate = [NSDate date];
     NSArray *proots = [self commitPersistentRootsWithEntityName: @"Person"
-                                                          count: NUM_PERSISTENT_ROOTS];
+                                                          count: NUM_PERSISTENT_ROOTS
+                                                   studentCount: 0];
 
     NSTimeInterval creationTime = [[NSDate date] timeIntervalSinceDate: startDate];
     NSLog(@"Time to commit %d new persistent roots: %0.2fs", NUM_PERSISTENT_ROOTS, creationTime);
@@ -115,7 +125,7 @@
 
     for (int commit = 0; commit < (NUM_COMMITS - 1); commit++)
     {
-        [self makeCommitToPersistentRoots: proots];
+        [self makeCommitToPersistentRoots: proots studentCount: 0];
     }
 
     NSTimeInterval commitTime = [[NSDate date] timeIntervalSinceDate: startDate];
@@ -124,7 +134,7 @@
     
     NSInteger __block migrationCounter = 0;
 
-    [ctx.store migrateRevisionsToVersion: 1 
+    [ctx.store migrateRevisionsToVersion: 1
                              withHandler: ^COItemGraph *(COItemGraph *oldItemGraph, int64_t oldVersion, int64_t newVersion)
      {
         NSMutableArray *newItems = [NSMutableArray array];
@@ -132,14 +142,108 @@
         for (COItem *oldItem in oldItemGraph.items)
         {
             ETAssert(oldVersion == oldItem.packageVersion);
-
             COMutableItem *newItem = [oldItem mutableCopy];
-            
             newItem.packageVersion = newVersion;
-
             [newItems addObject: newItem];
         }
-        
+        migrationCounter += 1;
+
+        return [[COItemGraph alloc] initWithItems: newItems
+                                     rootItemUUID: oldItemGraph.rootItemUUID];
+    }];
+    
+    UKIntsEqual(NUM_PERSISTENT_ROOTS * NUM_COMMITS, revCounter);
+    UKIntsEqual(revCounter, migrationCounter);
+
+    const NSTimeInterval rewriteTime = [[NSDate date] timeIntervalSinceDate: startDate];
+    NSLog(@"Time to rewrite %d store revisions: %0.2fs", revCounter, rewriteTime);
+}
+
+- (void)testFixedItemCount
+{
+    ctx.recordingUndo = NO;
+
+    NSDate *startDate = [NSDate date];
+    NSArray *proots = [self commitPersistentRootsWithEntityName: @"Person"
+                                                          count: NUM_PERSISTENT_ROOTS
+                                                   studentCount: 50];
+
+    NSTimeInterval creationTime = [[NSDate date] timeIntervalSinceDate: startDate];
+    NSLog(@"Time to commit %d new persistent roots: %0.2fs", NUM_PERSISTENT_ROOTS, creationTime);
+    startDate = [NSDate date];
+
+    for (int commit = 0; commit < (NUM_COMMITS - 1); commit++)
+    {
+        [self makeCommitToPersistentRoots: proots studentCount: 0];
+    }
+
+    NSTimeInterval commitTime = [[NSDate date] timeIntervalSinceDate: startDate];
+    NSLog(@"Time to make %d commits: %0.2fs", (NUM_COMMITS - 1), commitTime);
+    startDate = [NSDate date];
+    
+    NSInteger __block migrationCounter = 0;
+
+    [ctx.store migrateRevisionsToVersion: 1
+                             withHandler: ^COItemGraph *(COItemGraph *oldItemGraph, int64_t oldVersion, int64_t newVersion)
+     {
+        NSMutableArray *newItems = [NSMutableArray array];
+
+        for (COItem *oldItem in oldItemGraph.items)
+        {
+            ETAssert(oldVersion == oldItem.packageVersion);
+            COMutableItem *newItem = [oldItem mutableCopy];
+            newItem.packageVersion = newVersion;
+            [newItems addObject: newItem];
+        }
+        migrationCounter += 1;
+
+        return [[COItemGraph alloc] initWithItems: newItems
+                                     rootItemUUID: oldItemGraph.rootItemUUID];
+    }];
+    
+    UKIntsEqual(NUM_PERSISTENT_ROOTS * NUM_COMMITS, revCounter);
+    UKIntsEqual(revCounter, migrationCounter);
+
+    const NSTimeInterval rewriteTime = [[NSDate date] timeIntervalSinceDate: startDate];
+    NSLog(@"Time to rewrite %d store revisions: %0.2fs", revCounter, rewriteTime);
+}
+
+- (void)testGrowingItemCount
+{
+    ctx.recordingUndo = NO;
+
+    NSDate *startDate = [NSDate date];
+    NSArray *proots = [self commitPersistentRootsWithEntityName: @"Person"
+                                                          count: NUM_PERSISTENT_ROOTS
+                                                   studentCount: 0];
+
+    NSTimeInterval creationTime = [[NSDate date] timeIntervalSinceDate: startDate];
+    NSLog(@"Time to commit %d new persistent roots: %0.2fs", NUM_PERSISTENT_ROOTS, creationTime);
+    startDate = [NSDate date];
+
+    for (int commit = 0; commit < (NUM_COMMITS - 1); commit++)
+    {
+        [self makeCommitToPersistentRoots: proots studentCount: 1];
+    }
+
+    NSTimeInterval commitTime = [[NSDate date] timeIntervalSinceDate: startDate];
+    NSLog(@"Time to make %d commits: %0.2fs", (NUM_COMMITS - 1), commitTime);
+    startDate = [NSDate date];
+    
+    NSInteger __block migrationCounter = 0;
+
+    [ctx.store migrateRevisionsToVersion: 1
+                             withHandler: ^COItemGraph *(COItemGraph *oldItemGraph, int64_t oldVersion, int64_t newVersion)
+     {
+        NSMutableArray *newItems = [NSMutableArray array];
+
+        for (COItem *oldItem in oldItemGraph.items)
+        {
+            ETAssert(oldVersion == oldItem.packageVersion);
+            COMutableItem *newItem = [oldItem mutableCopy];
+            newItem.packageVersion = newVersion;
+            [newItems addObject: newItem];
+        }
         migrationCounter += 1;
 
         return [[COItemGraph alloc] initWithItems: newItems
